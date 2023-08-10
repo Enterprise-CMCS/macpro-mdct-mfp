88221c0e72833ffe94216f1e3511fd4fea34c395
Britt <britt.bujacich@gmail.com>
2023-08-10 11:01:11 -0600

    Mdct 2746 - add sidebar component (#42)

---
+++ services/ui-src/src/types/reportContext.ts
@@ -0,0 +1,75 @@
+// REPORT PROVIDER/CONTEXT
+
+import { AnyObject, ReportJson } from "types";
+
+export interface ReportKeys {
+  reportType: string;
+  state: string;
+  id: string;
+}
+
+export interface ReportMetadataShape extends ReportKeys {
+  submissionCount?: number;
+  reportType: string;
+  programName: string;
+  submissionName?: string;
+  status: ReportStatus;
+  reportingPeriodStartDate: number;
+  reportingPeriodEndDate: number;
+  dueDate: number;
+  createdAt: number;
+  lastAltered: number;
+  lastAlteredBy: string;
+  combinedData: boolean;
+  submittedBy?: string;
+  submitterEmail?: string;
+  submittedOnDate?: number;
+  archived?: boolean;
+  locked?: boolean;
+}
+
+export interface ReportShape extends ReportMetadataShape {
+  formTemplate: ReportJson;
+  fieldData: AnyObject;
+  completionStatus?: CompletionData;
+  isComplete?: boolean;
+}
+
+export interface CompletionData {
+  [key: string]: boolean | CompletionData;
+}
+
+export interface ReportContextMethods {
+  fetchReport: Function;
+  fetchReportsByState: Function;
+  archiveReport: Function;
+  releaseReport?: Function;
+  submitReport: Function;
+  createReport: Function;
+  updateReport: Function;
+  clearReportSelection: Function;
+  clearReportsByState: Function;
+  setReportSelection: Function;
+}
+
+export interface ReportContextShape extends ReportContextMethods {
+  report: ReportShape | undefined;
+  contextIsLoaded: boolean;
+  reportsByState: ReportMetadataShape[] | undefined;
+  errorMessage?: string | undefined;
+  lastSavedTime?: string | undefined;
+  isReportPage: boolean;
+}
+
+export enum ReportStatus {
+  NOT_STARTED = "Not started",
+  IN_PROGRESS = "In progress",
+  SUBMITTED = "Submitted",
+}
+
+export interface ReportPageProgress {
+  name: string;
+  path: string;
+  children?: ReportPageProgress[];
+  status?: boolean;
+}
