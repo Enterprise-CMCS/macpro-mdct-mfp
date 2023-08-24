547020ebd6dfe63e1233e5055aeb9a6f4feee6c4
Nicholas Summers <nick@coforma.io>
2023-08-24 13:20:49 -0400

    Remove test example from report page

--- services/ui-src/src/components/reports/StandardReportPage.tsx
+++ services/ui-src/src/components/reports/StandardReportPage.tsx
@@ -1,63 +1,16 @@
 // components
-import { Box, Button } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import { Form, ReportPageFooter, ReportPageIntro } from "components";
 // types
-import { AnyObject, ReportStatus, StandardReportPageShape } from "types";
-// utils
-import { getReportsByState, postReport } from "utils/api/requestMethods/report";
+import { StandardReportPageShape } from "types";
+// verbiage
 import { mockStandardReportPageJson } from "utils/testing/mockForm";
 
 export const StandardReportPage = ({ route, validateOnRender }: Props) => {
   const submitting = false;
-
-  const reportMetaData = {
-    metadata: {
-      programName: "testProgram",
-      reportType: "WP",
-      status: ReportStatus.NOT_STARTED,
-      isComplete: false,
-      createdAt: 162515200000,
-      lastAlteredBy: "Thelonious States",
-    },
-    fieldData: {
-      programName: "testProgram",
-    },
-  };
-
-  const createReport = async (
-    reportType: string,
-    state: string,
-    report: AnyObject
-  ) => {
-    try {
-      await postReport(reportType, state, report);
-      console.log("Post succeeded");
-    } catch (e: any) {
-      console.error("Unable to create report");
-    }
-  };
-
-  const fetchReportsByState = async (
-    reportType: string,
-    selectedState: string
-  ) => {
-    try {
-      const result = await getReportsByState(reportType, selectedState);
-      console.log(result);
-    } catch (e: any) {
-      console.error("Unable to fetch reports");
-    }
-  };
-
   return (
     <Box>
       {route.verbiage.intro && <ReportPageIntro text={route.verbiage.intro} />}
-      <Button onClick={() => createReport("WP", "NJ", reportMetaData)}>
-        Create Report
-      </Button>
-      <button onClick={() => fetchReportsByState("WP", "NJ")}>
-        Fetch Reports By State
-      </button>
       <Form
         id={route.form.id}
         formJson={route.form}
