209d3cb65dd74ea4cceacfe8ca4e8208245c860b
Nick Summers <ntsummers1@gmail.com>
2023-08-29 11:32:51 -0400

    API Handler - Fetch/Update Report (#70)
    Co-authored-by: Nicholas Summers <nick@coforma.io>

--- services/ui-src/src/utils/api/requestMethods/report.ts
+++ services/ui-src/src/utils/api/requestMethods/report.ts
@@ -1,5 +1,5 @@
 import { API } from "aws-amplify";
-import { AnyObject } from "types";
+import { AnyObject, ReportKeys } from "types";
 import { getRequestHeaders } from "./getRequestHeaders";
 import { updateTimeout } from "utils";
 
@@ -13,6 +13,22 @@ async function getReportsByState(reportType: string, state: string) {
   const response = await API.get(
     "mfp",
     `/reports/${reportType}/${state}`,
+    request
+  );
+  return response;
+}
+
+async function getReport(reportKeys: ReportKeys) {
+  const requestHeaders = await getRequestHeaders();
+  const request = {
+    headers: { ...requestHeaders },
+  };
+  const { reportType, state, id } = reportKeys;
+
+  updateTimeout();
+  const response = await API.get(
+    "mfp",
+    `/reports/${reportType}/${state}/${id}`,
     request
   );
   return response;
@@ -41,4 +57,21 @@ async function postReport(
   return response;
 }
 
-export { postReport, getReportsByState };
+async function putReport(reportKeys: ReportKeys, report: AnyObject) {
+  const requestHeaders = await getRequestHeaders();
+  const request = {
+    headers: { ...requestHeaders },
+    body: { ...report },
+  };
+  const { reportType, state, id } = reportKeys;
+
+  updateTimeout();
+  const response = await API.put(
+    "mfp",
+    `/reports/${reportType}/${state}/${id}`,
+    request
+  );
+  return response;
+}
+
+export { getReport, getReportsByState, postReport, putReport };
