d6eee0fd707287e670ab6875803ad3b9f43704f3
Nathan Keeys <nathankeeys@coformaatekeeys.myfiosgateway.com>
2023-08-30 12:17:05 -0400

    remove placeholder nav route

--- services/ui-src/src/components/menus/Sidebar.tsx
+++ services/ui-src/src/components/menus/Sidebar.tsx
@@ -70,14 +70,6 @@ export const Sidebar = ({ isHidden }: SidebarProps) => {
             section={{ name: "placeholder", path: "/standard" }}
             level={1}
           />
-          <NavSection
-            key={"section name"}
-            section={{
-              name: "Transition Benchmark Strategy",
-              path: "/wp/transition-benchmark-strategy",
-            }}
-            level={1}
-          />
         </Box>
       </Box>
     </>
