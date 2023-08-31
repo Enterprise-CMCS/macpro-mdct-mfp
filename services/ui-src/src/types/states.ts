4a2861287794c023626cc4d2f4e888b6cce48fe5
Karla Valcárcel Martínez <99458559+karla-vm@users.noreply.github.com>
2023-08-31 11:45:07 -0400

    Setup for Reports and Admin Banner Stores (#69)

--- services/ui-src/src/types/states.ts
+++ services/ui-src/src/types/states.ts
@@ -1,4 +1,9 @@
-import { MFPUser } from "types";
+import {
+  AdminBannerData,
+  MFPUser,
+  ReportMetadataShape,
+  ReportShape,
+} from "types";
 
 // initial user state
 export interface MfpUserState {
@@ -8,4 +13,32 @@ export interface MfpUserState {
   // ACTIONS
   setUser: (newUser: MFPUser | null) => void;
   setShowLocalLogins: (showLocalLogins: boolean) => void;
+}
+
+// initial admin banner state
+export interface AdminBannerState {
+  // INITIAL STATE
+  bannerData: AdminBannerData | undefined;
+  isBannerActive: boolean;
+  // ACTIONS
+  setAdminBanner: (newBannerData: AdminBannerData | undefined) => void;
+  clearAdminBanner: () => void;
+  setIsBannerActive: (bannerStatus: boolean) => void;
+}
+
+// initial report state
+export interface MfpReportState {
+  // INITIAL STATE
+  report: ReportShape | undefined;
+  reportsByState: ReportMetadataShape[] | undefined;
+  submittedReportsByState: ReportMetadataShape[] | undefined;
+  // ACTIONS
+  setReport: (newReport: ReportShape | undefined) => void;
+  setReportsByState: (
+    newReportsByState: ReportMetadataShape[] | undefined
+  ) => void;
+  clearReportsByState: () => void;
+  setSubmittedReportsByState: (
+    newSubmittedReportsByState: ReportMetadataShape[] | undefined
+  ) => void;
 }
