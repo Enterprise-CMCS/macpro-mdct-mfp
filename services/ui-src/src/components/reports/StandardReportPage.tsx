35473a2f875858ed61a76687fff528bc111be3a8
Garrett <57802560+gmrabian@users.noreply.github.com>
2023-09-06 09:56:42 -0400

    add routing to report page footer and other uses (#83)
    Co-authored-by: Garrett Rabian <garrett@coforma.io>

--- services/ui-src/src/components/reports/StandardReportPage.tsx
+++ services/ui-src/src/components/reports/StandardReportPage.tsx
@@ -1,13 +1,27 @@
+import { useNavigate } from "react-router-dom";
 // components
 import { Box } from "@chakra-ui/react";
 import { Form, ReportPageFooter, ReportPageIntro } from "components";
 // types
 import { StandardReportPageShape } from "types";
+// utils
+import { useFindRoute, useStore } from "utils";
 // verbiage
 import { mockStandardReportPageJson } from "utils/testing/mockForm";
 
 export const StandardReportPage = ({ route, validateOnRender }: Props) => {
   const submitting = false;
+  const navigate = useNavigate();
+  const report = useStore().report;
+  const { nextRoute } = useFindRoute(
+    report?.formTemplate.flatRoutes!,
+    report?.formTemplate.basePath
+  );
+
+  const onError = () => {
+    navigate(nextRoute);
+  };
+
   return (
     <Box>
       {route.verbiage.intro && <ReportPageIntro text={route.verbiage.intro} />}
@@ -15,6 +29,7 @@ export const StandardReportPage = ({ route, validateOnRender }: Props) => {
         id={route.form.id}
         formJson={route.form}
         onSubmit={() => {}}
+        onError={onError}
         formData={mockStandardReportPageJson.form}
         autosave
         validateOnRender={validateOnRender || false}
