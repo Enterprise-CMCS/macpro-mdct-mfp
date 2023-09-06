62c6bb45a1b5eeef2c128dc629390e02cd8d7a92
ailZhou <aileen.zhou@coforma.io>
2023-09-06 16:42:58 -0400

    fix alert title

--- services/ui-src/src/verbiage/pages/wp/wp-alerts.ts
+++ services/ui-src/src/verbiage/pages/wp/wp-alerts.ts
@@ -1,6 +1,6 @@
 export default {
   initiatives: {
-    title: "Your form is not ready for submission",
+    title: "Your initiatives do not meet the topic requirements",
     description:
       "Initiatives must meet the following requirements at least once across all the initiatives: <li>Transitions and transition coordination services</li><li>Housing-related supports</li><li>Quality measurement and improvement</li><li>Self-direction (If applicable)</li><li>Tribal (if applicable)</li>To correct this, ensure you add at least one initiative for each of these topics. This alert will disappear once they're met. ",
   },
