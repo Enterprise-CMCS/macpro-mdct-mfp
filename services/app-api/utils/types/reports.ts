5fe6afc087abf3c6e92b956da6b2fe532a496446
Karla Valcárcel Martínez <99458559+karla-vm@users.noreply.github.com>
2023-09-28 16:07:55 -0400

    sar.json Initial Buildout (#107)
    Co-authored-by: benmartin-coforma <126210497+benmartin-coforma@users.noreply.github.com>

--- services/app-api/utils/types/reports.ts
+++ services/app-api/utils/types/reports.ts
@@ -96,7 +96,7 @@ export interface ReportPageVerbiage {
   intro: {
     section: string;
     subsection?: string;
-    spreadsheet?: string;
+    hint?: string;
     info?: string | CustomHtmlElement[];
   };
 }
