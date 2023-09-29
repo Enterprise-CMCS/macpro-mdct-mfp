f3b05a10b26290550d5e50e66fe551c72a081c3c
Ajaita Saini <ajaita.saini@coforma.io>
2023-09-29 12:11:39 -0400

    autosave logic

--- services/ui-src/src/components/overlays/EntityDetailsOverlay.tsx
+++ services/ui-src/src/components/overlays/EntityDetailsOverlay.tsx
@@ -14,8 +14,11 @@ import { AlertTypes, EntityDetailsOverlayShape } from "types";
 import closeIcon from "assets/icons/icon_cancel_x_white.png";
 import arrowLeftBlue from "assets/icons/icon_arrow_left_blue.png";
 import warningIcon from "assets/icons/icon_warning.png";
+// verbiage
+import { useStore } from "utils";
 
 export const EntityDetailsOverlay = ({ route, validateOnRender }: Props) => {
+  const { report } = useStore();
   const submitting = false;
   const { form, verbiage } = route;
 
@@ -51,7 +54,8 @@ export const EntityDetailsOverlay = ({ route, validateOnRender }: Props) => {
         id={form.id}
         formJson={form}
         onSubmit={() => {}}
-        autosave
+        autosave={true}
+        formData={report?.fieldData}
         validateOnRender={validateOnRender || false}
         dontReset={false}
       />
