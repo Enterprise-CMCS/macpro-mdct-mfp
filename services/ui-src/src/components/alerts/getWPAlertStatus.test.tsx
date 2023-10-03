b23e3e4b6cf59eb4df622500aac9575622837d68
ailZhou <127151429+ailZhou@users.noreply.github.com>
2023-10-03 17:00:23 -0400

    WP Initiative Alert Cleanup (#132)

--- services/ui-src/src/components/alerts/getWPAlertStatus.test.tsx
+++ services/ui-src/src/components/alerts/getWPAlertStatus.test.tsx
@@ -2,6 +2,18 @@ import { ReportShape } from "types";
 import { getWPAlertStatus } from "./getWPAlertStatus";
 
 describe("Test WP Alerts", () => {
+  const topics = [
+    "Transitions and transition coordination services*",
+    "Housing-related supports*",
+    "Quality measurement and improvement*",
+    "Self-direction (*if applicable)",
+    "Tribal Initiative (*if applicable)",
+  ];
+
+  const initiative_wpTopic = topics.map((topic) => {
+    return { value: topic };
+  });
+
   test("Test checkInitiativesTopics not filled", () => {
     const report: ReportShape = {
       fieldData: {
@@ -17,20 +29,10 @@ describe("Test WP Alerts", () => {
     expect(getWPAlertStatus(report, "initiatives")).toBeTruthy();
   });
   test("Test checkInitiativesTopics filled", () => {
-    const topics = [
-      "Transitions and transition coordination services*",
-      "Housing-related supports*",
-      "Quality measurement and improvement*",
-      "Self-direction(*if applicable)",
-      "Tribal Initiative (*if applicable)",
-    ];
-
-    const initiative_wpTopic = topics.map((topic) => {
-      return { value: topic };
-    });
-
     const report: ReportShape = {
       fieldData: {
+        firstquestion: { value: "Yes" },
+        secondquestion: { value: "Yes" },
         initiatives: [
           {
             initiative_wpTopic: initiative_wpTopic,
@@ -41,5 +43,19 @@ describe("Test WP Alerts", () => {
 
     //turn off alertbox
     expect(getWPAlertStatus(report, "initiatives")).toBeFalsy();
+  });
+  test("Test checkInitiativesTopics if previous questions were not fileed", () => {
+    const report: ReportShape = {
+      fieldData: {
+        initiatives: [
+          {
+            initiative_wpTopic: initiative_wpTopic,
+          },
+        ],
+      },
+    } as unknown as ReportShape;
+
+    //alertbox will still be active
+    expect(getWPAlertStatus(report, "initiatives")).toBeTruthy();
   });
 });
