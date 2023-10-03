4c88ad1ce6b35adc684277de897234615ed5ae0e
Nathan Keeys <nathankeeys@Coforma-YWQ6Q36WM0-Nate-Keeys.local>
2023-10-02 23:59:07 -0400

    add dynamic choices for define initiative total populations choice list

--- services/ui-src/src/components/fields/CheckboxField.tsx
+++ services/ui-src/src/components/fields/CheckboxField.tsx
@@ -2,6 +2,7 @@
 import { Box, Heading } from "@chakra-ui/react";
 import { ChoiceListField } from "components";
 // utils
+import { useStore } from "utils";
 import { ChoiceFieldProps } from "types";
 
 export const CheckboxField = ({
@@ -12,6 +13,42 @@ export const CheckboxField = ({
   sxOverride,
   ...props
 }: ChoiceFieldProps) => {
+  const { report } = useStore();
+
+  const getDynamicChoices = () => {
+    const isTargetPopulationChoices = choices.filter((choice) =>
+      choice.id.match("targetPopulations")
+    );
+    // is target population choices the only choices that need to be dynamically pulled?
+    if (isTargetPopulationChoices) {
+      const targetPopulationChoices = report?.fieldData.targetPopulation;
+
+      const labelCustomTargetPopulationChoice = (choice: any) => {
+        if (targetPopulationChoices.indexOf(choice) >= 4) {
+          return `Other: {${choice.transitionBenchmarks_targetPopulationName}}`;
+        } else {
+          return choice.transitionBenchmarks_targetPopulationName;
+        }
+      };
+
+      const formatTargetPopulationChoices = targetPopulationChoices.map(
+        (choice: any) => {
+          return {
+            checked: choice.checked,
+            id: choice.id,
+            label: labelCustomTargetPopulationChoice(choice),
+            name: choice.transitionBenchmarks_targetPopulationName,
+            value: choice.transitionBenchmarks_targetPopulationName,
+          };
+        }
+      );
+
+      return formatTargetPopulationChoices;
+    } else {
+      return choices;
+    }
+  };
+
   return (
     <Box sx={sxOverride}>
       {/* SAR field sections */}
@@ -20,7 +57,7 @@ export const CheckboxField = ({
         type="checkbox"
         name={name}
         label={label}
-        choices={choices}
+        choices={getDynamicChoices()}
         {...props}
       />
     </Box>
