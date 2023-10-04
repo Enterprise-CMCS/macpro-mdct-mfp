b11e25a67742bd2dcec2a1c23263ffc2decea497
Nicholas Summers <nick@coforma.io>
2023-10-04 17:02:45 -0400

    update metadata validation for sar api creation handler

--- services/ui-src/src/types/reportContext.ts
+++ services/ui-src/src/types/reportContext.ts
@@ -23,6 +23,7 @@ export interface ReportMetadataShape extends ReportKeys {
   locked?: boolean;
   dueDate: number;
   reportPeriod: number;
+  finalSar?: boolean;
 }
 
 export interface ReportShape extends ReportMetadataShape {
