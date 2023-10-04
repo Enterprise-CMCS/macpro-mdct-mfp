fef609c017481842a2ee7c74cc3933f8a19e06b1
brittbujacich <britt.bujacich@gmail.com>
2023-10-04 17:56:16 -0600

    add admin view instructions to dashboard

--- services/ui-src/src/verbiage/pages/accordion.ts
+++ services/ui-src/src/verbiage/pages/accordion.ts
@@ -6,53 +6,7 @@ export default {
         {
           type: "text",
           as: "header",
-          content: "State or Territory User Instructions",
-        },
-        {
-          type: "text",
-          as: "span",
-          content:
-            "This reporting tool is to be used by grantees for reporting of MFP program data. The information provided in this report will allow CMS to monitor grantee progress and identify challenges and improvement opportunities. For additional guidance on completing this form, please see the associated ",
-        },
-        {
-          type: "externalLink",
-          content: "User Guide",
-          props: {
-            href: "https://www.ecfr.gov/current/title-42/chapter-IV/subchapter-C/part-438/subpart-A/section-438.8#p-438.8(k)",
-            target: "_blank",
-            ariaLabel: "Link opens in new tab",
-          },
-        },
-        {
-          type: "text",
-          as: "span",
-          content: " and ",
-        },
-        {
-          type: "externalLink",
-          content: "Help File",
-          props: {
-            href: "https://www.ecfr.gov/current/title-42/chapter-IV/subchapter-C/part-438/subpart-A/section-438.8#p-438.8(k)",
-            target: "_blank",
-            ariaLabel: "Link opens in new tab",
-          },
-        },
-        {
-          type: "text",
-          as: "span",
-          content: ".",
-        },
-      ],
-      list: [],
-      text: "",
-    },
-    stateUserDashboard: {
-      buttonLabel: "Instructions",
-      intro: [
-        {
-          type: "text",
-          as: "span",
-          content: "<strong>State or Territory User Instructions</strong><br>",
+          content: "<strong>State or Territory User Instructions<strong>",
         },
         {
           type: "text",
@@ -100,6 +54,47 @@ export default {
         "To archive a submission and hide it from a state or territory’s dashboard, use “Archive”.",
         "To approve a submission, review the submission, go to the Review & Submit page and select “Approve”. The status will change to “Approved” and the content will be eligible for import into the SAR.",
       ],
+      text: "",
+    },
+    stateUserDashboard: {
+      buttonLabel: "Instructions",
+      intro: [
+        {
+          type: "text",
+          as: "span",
+          content:
+            "This reporting tool is to be used by grantees for reporting of MFP program data. The information provided in this report will allow CMS to monitor grantee progress and identify challenges and improvement opportunities. For additional guidance on completing this form, please see the associated ",
+        },
+        {
+          type: "externalLink",
+          content: "User Guide",
+          props: {
+            href: "https://www.ecfr.gov/current/title-42/chapter-IV/subchapter-C/part-438/subpart-A/section-438.8#p-438.8(k)",
+            target: "_blank",
+            ariaLabel: "Link opens in new tab",
+          },
+        },
+        {
+          type: "text",
+          as: "span",
+          content: " and ",
+        },
+        {
+          type: "externalLink",
+          content: "Help File",
+          props: {
+            href: "https://www.ecfr.gov/current/title-42/chapter-IV/subchapter-C/part-438/subpart-A/section-438.8#p-438.8(k)",
+            target: "_blank",
+            ariaLabel: "Link opens in new tab",
+          },
+        },
+        {
+          type: "text",
+          as: "span",
+          content: ".<br>",
+        },
+      ],
+      list: [],
       text: "",
     },
     formIntro: {
