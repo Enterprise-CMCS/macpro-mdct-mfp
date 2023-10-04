7352cd727b807fcb59a79c1700df38e9ac6a58f4
Nicholas Summers <nick@coforma.io>
2023-10-04 17:03:10 -0400

    Show edit button for sar

--- services/ui-src/src/components/pages/Dashboard/DashboardTable.tsx
+++ services/ui-src/src/components/pages/Dashboard/DashboardTable.tsx
@@ -32,7 +32,9 @@ export const DashboardTable = ({
     {reportsByState.map((report: ReportMetadataShape) => (
       <Tr key={report.id}>
         {/* Edit Button */}
-        {isStateLevelUser && !report?.locked ? (
+        {isStateLevelUser &&
+        !report?.locked &&
+        reportType === ReportType.SAR ? (
           <EditReportButton
             report={report}
             openAddEditReportModal={openAddEditReportModal}
