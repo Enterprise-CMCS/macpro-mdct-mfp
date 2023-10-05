cee0c2f6d0e6e7f57a1430dc71709500bb4522a5
Ajaita Saini <ajaita.saini@coforma.io>
2023-10-05 10:00:11 -0400

    submit close out initiative

--- services/ui-src/src/components/modals/CloseEntityModal.tsx
+++ services/ui-src/src/components/modals/CloseEntityModal.tsx
@@ -1,22 +1,45 @@
+import { useState, useContext } from "react";
 // components
-import { Modal } from "components";
+import { Modal, ReportContext } from "components";
 // types
 import { AnyObject, EntityShape } from "types";
 //utils
-import { renderHtml } from "utils";
+import { renderHtml, useStore } from "utils";
 
 export const CloseEntityModal = ({ verbiage, modalDisclosure }: Props) => {
-  const closing = false;
-  const closeProgramHandler = async () => {
+  const { report } = useStore();
+  const [closingOut, setClosingOut] = useState<boolean>(false);
+  const modalInfo = verbiage.closeOutModal;
+  const { full_name, state } = useStore().user ?? {};
+  const { updateReport } = useContext(ReportContext);
+
+  const closeOutProgramHandler = async () => {
+    setClosingOut(true);
+
+    const reportKeys = {
+      reportType: report?.reportType,
+      state: state,
+      id: report?.id,
+    };
+
+    const dataToWrite = {
+      metadata: {
+        isInitiativeClosedOut: true,
+        lastAltered: Date.now(),
+        lastAlteredBy: full_name,
+      },
+    };
+    await updateReport(reportKeys, dataToWrite);
+
+    setClosingOut(false);
     modalDisclosure.onClose();
   };
-  const modalInfo = verbiage.closeOutModal;
 
   return (
     <Modal
-      onConfirmHandler={closeProgramHandler}
+      onConfirmHandler={closeOutProgramHandler}
       modalDisclosure={modalDisclosure}
-      submitting={closing}
+      submitting={closingOut}
       content={{
         heading: modalInfo.closeOutModalTitle,
         actionButtonText: modalInfo.closeOutModalConfirmButtonText,
