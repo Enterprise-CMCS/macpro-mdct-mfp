b386465570eb056049dc3a8415000e65fc79e0ab
ailZhou <127151429+ailZhou@users.noreply.github.com>
2023-10-11 13:26:17 -0400

    WP Admin Dashboard (#141)

--- services/ui-src/src/components/pages/Dashboard/DashboardTable.tsx
+++ services/ui-src/src/components/pages/Dashboard/DashboardTable.tsx
@@ -32,7 +32,7 @@ export const DashboardTable = ({
     {reportsByState.map((report: ReportMetadataShape) => (
       <Tr key={report.id}>
         {/* Edit Button */}
-        {isStateLevelUser && !report?.locked ? (
+        {isStateLevelUser && reportType === "SAR" && !report?.locked ? (
           <EditReportButton
             report={report}
             openAddEditReportModal={openAddEditReportModal}
@@ -44,7 +44,7 @@ export const DashboardTable = ({
         {/* Report Name */}
         <Td sx={sxOverride.submissionNameText}>{report.submissionName}</Td>
         {/* Date Fields */}
-        <DateFields report={report} reportType={reportType} />
+        <DateFields report={report} reportType={reportType} isAdmin={isAdmin} />
         {/* Last Altered By */}
         <Td>{report?.lastAlteredBy || "-"}</Td>
         {/* Report Status */}
@@ -56,9 +56,13 @@ export const DashboardTable = ({
             report.submissionCount
           )}
         </Td>
-        {/* SAR-ONLY: Submission count */}
-        {reportType === "SAR" && isAdmin && (
-          <Td> {report.submissionCount === 0 ? 1 : report.submissionCount} </Td>
+        {/* Admin: Submission count */}
+        {isAdmin && (
+          <Td>
+            {!report.submissionCount || report.submissionCount === 0
+              ? 1
+              : report.submissionCount}
+          </Td>
         )}
         {/* Action Buttons */}
         <Td sx={sxOverride.editReportButtonCell}>
@@ -70,7 +74,7 @@ export const DashboardTable = ({
           >
             {entering && reportId == report.id ? (
               <Spinner size="md" />
-            ) : isStateLevelUser ? (
+            ) : isStateLevelUser && !report?.locked ? (
               "Edit"
             ) : (
               "View"
@@ -79,7 +83,7 @@ export const DashboardTable = ({
         </Td>
         {isAdmin && (
           <>
-            {reportType === "SAR" && (
+            {
               <AdminReleaseButton
                 report={report}
                 reportType={reportType}
@@ -88,7 +92,7 @@ export const DashboardTable = ({
                 releasing={releasing}
                 sxOverride={sxOverride}
               />
-            )}
+            }
             <AdminArchiveButton
               report={report}
               reportType={reportType}
@@ -148,6 +152,10 @@ const tableBody = (body: TableContentShape, isAdmin: boolean) => {
   if (!isAdmin) {
     tableContent.headRow = tableContent.headRow!.filter((e) => e !== "#");
     return tableContent;
+  } else {
+    tableContent.headRow = tableContent.headRow!.filter(
+      (e) => e !== "Due date"
+    );
   }
   return body;
 };
@@ -172,10 +180,12 @@ interface EditReportProps {
   sxOverride: AnyObject;
 }
 
-const DateFields = ({ report, reportType }: DateFieldProps) => {
+const DateFields = ({ report, reportType, isAdmin }: DateFieldProps) => {
   return (
     <>
-      {reportType === "WP" && <Td>{convertDateUtcToEt(report.createdAt)}</Td>}
+      {reportType === "WP" && !isAdmin && (
+        <Td>{convertDateUtcToEt(report.createdAt)}</Td>
+      )}
       <Td>{convertDateUtcToEt(report.lastAltered)}</Td>
     </>
   );
@@ -184,6 +194,7 @@ const DateFields = ({ report, reportType }: DateFieldProps) => {
 interface DateFieldProps {
   report: ReportMetadataShape;
   reportType: string;
+  isAdmin: boolean;
 }
 
 const AdminReleaseButton = ({
@@ -193,11 +204,22 @@ const AdminReleaseButton = ({
   releaseReport,
   sxOverride,
 }: AdminActionButtonProps) => {
+  //unlock is enabled when status: approved and submitted, all other times, it is disabled
+  const reportStatus = getStatus(
+    report.reportType as ReportType,
+    report.status,
+    report.archived,
+    report.submissionCount
+  );
+  const isDisabled = !(
+    reportStatus === "Submitted" || reportStatus === "Approved"
+  );
+
   return (
     <Td>
       <Button
         variant="link"
-        disabled={report.locked === false || report.archived === true}
+        disabled={isDisabled}
         sx={sxOverride.adminActionButton}
         onClick={() => releaseReport!(report)}
       >
