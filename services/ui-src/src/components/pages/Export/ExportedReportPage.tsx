483dc3b2b2fc7164ee4086a2999435abf408d5d4
Casie Siekman <casiesiekman@coformaesiekman.lan>
2023-10-17 14:52:28 -0500

    show what report this PDF is for

--- services/ui-src/src/components/pages/Export/ExportedReportPage.tsx
+++ services/ui-src/src/components/pages/Export/ExportedReportPage.tsx
@@ -1,3 +1,10 @@
+// utils
+import { useStore } from "utils";
+import { ReportType } from "types";
 export const ExportedReportPage = () => {
-  return <div>Hi! This will be a PDF</div>;
+  const { report } = useStore() ?? {};
+  const reportType = (report?.reportType ||
+    localStorage.getItem("selectedReportType")) as ReportType;
+
+  return <div>Hi! This will be a PDF for {reportType}</div>;
 };
