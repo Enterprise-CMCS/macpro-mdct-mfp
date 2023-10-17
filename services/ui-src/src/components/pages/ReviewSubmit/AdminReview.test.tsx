2edc24d85a14cae4506b0dc9766f8cf98b5b8914
Nathan K <keeysnc@gmail.com>
2023-10-17 13:13:28 -0400

    MDCT -  WP Admin Review and Submit (UI only) (#161)
    Co-authored-by: Nathan Keeys <nathankeeys@coformaatekeeys.myfiosgateway.com>
    Co-authored-by: Nathan Keeys <nathankeeys@Coforma-YWQ6Q36WM0-Nate-Keeys.local>

---
+++ services/ui-src/src/components/pages/ReviewSubmit/AdminReview.test.tsx
@@ -0,0 +1,80 @@
+import { render, screen } from "@testing-library/react";
+// components
+import { ReportContext } from "components";
+// utils
+import {
+  mockAdminUserStore,
+  mockLDFlags,
+  mockReportMethods,
+  mockUseStore,
+  RouterWrappedComponent,
+} from "utils/testing/setupJest";
+import { useStore } from "utils";
+import reviewVerbiage from "verbiage/pages/mfp/mfp-review-and-submit";
+import { AdminReview } from "./AdminReview";
+// types
+import { ReportStatus } from "types";
+
+jest.mock("utils/state/useStore");
+const mockedUseStore = useStore as jest.MockedFunction<typeof useStore>;
+
+mockLDFlags.setDefault({ pdfExport: false });
+
+const WpReviewSubmitPage = (
+  <RouterWrappedComponent>
+    <ReportContext.Provider value={mockReportMethods}>
+      <AdminReview
+        reviewVerbiage={reviewVerbiage}
+        submitting={false}
+        submitForm={() => {}}
+      />
+    </ReportContext.Provider>
+  </RouterWrappedComponent>
+);
+
+describe("MFP Review and Submit Page Functionality", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe("Review and Submit Page - Admin View", () => {
+    test("Show admin view when admin user is logged in", () => {
+      mockedUseStore.mockReturnValue({
+        ...mockUseStore,
+        user: mockAdminUserStore,
+      });
+      render(WpReviewSubmitPage);
+      const { review } = reviewVerbiage;
+      const { adminInfo } = review;
+      expect(screen.getByText(adminInfo.submitLink.text)).toBeVisible();
+    });
+
+    test("Disable unlock and approve buttons when report is unlocked", () => {
+      mockedUseStore.mockReturnValue({
+        ...mockUseStore,
+        report: {
+          status: ReportStatus.IN_PROGRESS,
+        },
+        user: mockAdminUserStore,
+      });
+      render(WpReviewSubmitPage);
+      const { review } = reviewVerbiage;
+      const { adminInfo } = review;
+      expect(screen.getByText(adminInfo.submitLink.text)).toBeDisabled();
+    });
+
+    test("Enable unlock and approve buttons when report is locked", () => {
+      mockedUseStore.mockReturnValue({
+        ...mockUseStore,
+        report: {
+          status: ReportStatus.SUBMITTED,
+        },
+        user: mockAdminUserStore,
+      });
+      render(WpReviewSubmitPage);
+      const { review } = reviewVerbiage;
+      const { adminInfo } = review;
+      expect(screen.getByText(adminInfo.submitLink.text)).toBeEnabled();
+    });
+  });
+});
