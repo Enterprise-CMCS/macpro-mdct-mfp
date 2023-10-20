dd18e61b386efd7535a8797cf3ae570f387fae78
Karla Valcárcel Martínez <99458559+karla-vm@users.noreply.github.com>
2023-10-20 11:57:57 -0400

    MDCT-3018: RE&T Page Creation Pt. 1 (#171)

--- services/ui-src/src/utils/reports/entities.ts
+++ services/ui-src/src/utils/reports/entities.ts
@@ -1,4 +1,12 @@
-import { EntityShape, OverlayModalStepTypes, AnyObject } from "types";
+import {
+  EntityShape,
+  OverlayModalStepTypes,
+  AnyObject,
+  FormField,
+  FormLayoutElement,
+  ReportShape,
+  ReportType,
+} from "types";
 
 const getRadioValue = (entity: EntityShape | undefined, label: string) => {
   const radioLabelValue = entity?.[label]?.[0].value;
@@ -56,3 +64,70 @@ export const entityWasUpdated = (
   originalEntity: EntityShape,
   newEntity: AnyObject
 ) => JSON.stringify(originalEntity) !== JSON.stringify(newEntity);
+
+// rendering dynamic Target Population fields on the WP and SAR forms
+export const renderTargetPopulationFields = (
+  report: ReportShape | undefined,
+  fields: (FormField | FormLayoutElement)[],
+  location: AnyObject
+) => {
+  const updatedTargetPopulations =
+    report?.reportType === ReportType.SAR
+      ? report?.fieldData?.workPlanData.targetPopulations
+      : report?.fieldData?.targetPopulations;
+
+  // choice lists with target populations
+  const formattedFields = updatedTargetPopulations?.map(
+    (field: EntityShape) => {
+      if (report?.reportType === ReportType.SAR) {
+        const fieldDisplayValue =
+          field.transitionBenchmarks_targetPopulationName.match(/\(([^()]*)\)/)
+            ? field.transitionBenchmarks_targetPopulationName.match(
+                /\(([^()]*)\)/
+              )[1]
+            : field.transitionBenchmarks_targetPopulationName;
+
+        // number fields (RE&T sections)
+        return {
+          id: field.id,
+          type: "number",
+          validation: "number",
+          props: {
+            label: field.isRequired
+              ? `Number of ${fieldDisplayValue}`
+              : `Other: ${fieldDisplayValue}`,
+            name: field.transitionBenchmarks_targetPopulationName,
+            value: "",
+          },
+        };
+      }
+
+      // choice list fields (Work Plan sections)
+      return {
+        checked: false,
+        id: field.id,
+        label: field.isRequired
+          ? field.transitionBenchmarks_targetPopulationName
+          : `Other: ${field.transitionBenchmarks_targetPopulationName}`,
+        name: field.transitionBenchmarks_targetPopulationName,
+        value: field.transitionBenchmarks_targetPopulationName,
+      };
+    }
+  );
+
+  // only return target population text fields when rendering RE&T forms
+  if (location.pathname.startsWith("/sar/recruitment-enrollment-transitions")) {
+    return formattedFields;
+  }
+
+  const updatedTargetPopulationChoiceList = fields.map((field) => {
+    return field.id.match("targetPopulations")
+      ? {
+          ...field,
+          props: { ...field?.props, choices: [...formattedFields] },
+        }
+      : { ...field };
+  });
+
+  return updatedTargetPopulationChoiceList;
+};
