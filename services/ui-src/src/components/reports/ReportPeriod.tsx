2bcb8ff012c57529557feb5be21c723c1aa80df0
brittbujacich <britt.bujacich@gmail.com>
2023-10-20 11:59:32 -0600

    make the reporting period section its own component

---
+++ services/ui-src/src/components/reports/ReportPeriod.tsx
@@ -0,0 +1,39 @@
+// components
+import { Box, Heading } from "@chakra-ui/react";
+// utils
+import { displayLongformPeriod } from "utils";
+import { AnyObject } from "types";
+
+export const ReportPeriod = ({ text, reportPeriod }: Props) => {
+  const { section, subsection } = text;
+  const retSection = section === "Recruitment, Enrollment, and Transitions";
+  const pageNine = subsection.includes("HCBS");
+  const currentPeriod = displayLongformPeriod(reportPeriod);
+  return (
+    <Box>
+      {retSection &&
+        (!pageNine ? (
+          <Heading as="h2" sx={sx.periodText}>
+            {currentPeriod}
+          </Heading>
+        ) : (
+          <Heading as="h2" sx={sx.periodText}>
+            This page needs a different calculation
+          </Heading>
+        ))}
+    </Box>
+  );
+};
+
+interface Props {
+  text: AnyObject;
+  [key: string]: any;
+  reportPeriod?: number;
+}
+
+const sx = {
+  periodText: {
+    marginTop: "1.5rem",
+    fontSize: "2xl",
+  },
+};
