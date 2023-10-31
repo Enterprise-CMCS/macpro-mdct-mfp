9dc202097104c1db69bb431bf2d54d6ee30c92a9
Nathan K <keeysnc@gmail.com>
2023-10-31 10:45:19 -0400

    CMDCT 261 - Admin Review Functionality (#178)
    Co-authored-by: Nathan Keeys <nathankeeys@coformaatekeeys.myfiosgateway.com>
    Co-authored-by: Nathan Keeys <nathankeeys@Coforma-YWQ6Q36WM0-Nate-Keeys.local>

---
+++ services/app-api/handlers/reports/approve.test.ts
@@ -0,0 +1,97 @@
+import { fetchReport } from "./fetch";
+import { approveReport } from "./approve";
+import { APIGatewayProxyEvent } from "aws-lambda";
+// utils
+import { proxyEvent } from "../../utils/testing/proxyEvent";
+import { mockWPReport } from "../../utils/testing/setupJest";
+import { error } from "../../utils/constants/constants";
+// types
+import { StatusCodes } from "../../utils/types";
+
+jest.mock("../../utils/auth/authorization", () => ({
+  isAuthorized: jest.fn().mockResolvedValue(true),
+  hasPermissions: jest.fn(() => {}),
+}));
+
+const mockAuthUtil = require("../../utils/auth/authorization");
+
+jest.mock("../../utils/debugging/debug-lib", () => ({
+  init: jest.fn(),
+  flush: jest.fn(),
+}));
+
+jest.mock("./fetch");
+const mockedFetchReport = fetchReport as jest.MockedFunction<
+  typeof fetchReport
+>;
+
+const mockProxyEvent: APIGatewayProxyEvent = {
+  ...proxyEvent,
+  headers: { "cognito-identity-id": "test" },
+  pathParameters: { reportType: "WP", state: "NJ", id: "mock-report-id" },
+  body: JSON.stringify(mockWPReport),
+};
+
+const approveEvent: APIGatewayProxyEvent = {
+  ...mockProxyEvent,
+  body: JSON.stringify({
+    ...mockWPReport,
+    status: "Approved",
+  }),
+};
+
+describe("Test approveReport method", () => {
+  beforeEach(() => {
+    // fail state and pass admin auth checks
+    mockAuthUtil.hasPermissions
+      .mockReturnValueOnce(true)
+      .mockReturnValueOnce(true)
+      .mockReturnValueOnce(false);
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("Test approve report passes with valid data", async () => {
+    mockedFetchReport.mockResolvedValue({
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "string",
+        "Access-Control-Allow-Credentials": true,
+      },
+      body: JSON.stringify(mockWPReport),
+    });
+    const res: any = await approveReport(approveEvent, null);
+    const body = JSON.parse(res.body);
+    expect(res.statusCode).toBe(StatusCodes.SUCCESS);
+    expect(body.status).toBe("Approved");
+  });
+
+  test("Test approve report with no existing record throws 404", async () => {
+    mockedFetchReport.mockResolvedValue({
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "string",
+        "Access-Control-Allow-Credentials": true,
+      },
+      body: undefined!,
+    });
+    const res = await approveReport(approveEvent, null);
+    expect(res.statusCode).toBe(StatusCodes.NOT_FOUND);
+    expect(res.body).toContain(error.NO_MATCHING_RECORD);
+  });
+
+  test("Test approve report without admin permissions throws 403", async () => {
+    mockedFetchReport.mockResolvedValue({
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "string",
+        "Access-Control-Allow-Credentials": true,
+      },
+      body: undefined!,
+    });
+    const res = await approveReport(approveEvent, null);
+    expect(res.statusCode).toBe(StatusCodes.UNAUTHORIZED);
+    expect(res.body).toContain(error.UNAUTHORIZED);
+  });
+});
