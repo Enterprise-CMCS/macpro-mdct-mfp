dc195b54e3be468ff381d296a3461b231c5863b4
brittbujacich <britt.bujacich@gmail.com>
2023-11-16 15:09:50 -0700

    added comment and updated ratios

--- services/ui-src/src/utils/validation/schemas.ts
+++ services/ui-src/src/utils/validation/schemas.ts
@@ -1,7 +1,10 @@
 import { array, boolean, mixed, object, string } from "yup";
 import { validationErrors as error } from "verbiage/errors";
 import { Choice } from "types";
-import { checkStandardNumberInputAgainstRegexes } from "utils/other/checkInputValidity";
+import {
+  checkRatioInputAgainstRegexes,
+  checkStandardNumberInputAgainstRegexes,
+} from "utils/other/checkInputValidity";
 
 // TEXT - Helpers
 const isWhitespaceString = (value?: string) => value?.trim().length === 0;
@@ -19,12 +22,6 @@ export const textOptional = () => string().typeError(error.INVALID_GENERIC);
 
 // NUMBER - Helpers
 const validNAValues = ["N/A", "Data not available"];
-
-const valueCleaningNumberSchema = (value: string, charsToReplace: RegExp) => {
-  return numberSchema().transform((_value) => {
-    return Number(value.replace(charsToReplace, ""));
-  });
-};
 
 // NUMBER - Number or Valid Strings
 export const numberSchema = () =>
@@ -60,6 +57,7 @@ const validNumberSchema = () =>
     },
   });
 
+// this is in case there are any fields that should only accept integers
 export const validNumber = () =>
   validNumberSchema()
     .required(error.REQUIRED_GENERIC)
@@ -98,33 +96,7 @@ export const ratio = () =>
     .test({
       message: error.INVALID_RATIO,
       test: (val) => {
-        const replaceCharsRegex = /[,.:]/g;
-        const ratio = val?.split(":");
-
-        // Double check and make sure that a ratio contains numbers on both sides
-        if (
-          !ratio ||
-          ratio.length != 2 ||
-          ratio[0].trim().length == 0 ||
-          ratio[1].trim().length == 0
-        ) {
-          return false;
-        }
-
-        // Check if the left side of the ratio is a valid number
-        const firstTest = valueCleaningNumberSchema(
-          ratio[0],
-          replaceCharsRegex
-        ).isValidSync(val);
-
-        // Check if the right side of the ratio is a valid number
-        const secondTest = valueCleaningNumberSchema(
-          ratio[1],
-          replaceCharsRegex
-        ).isValidSync(val);
-
-        // If both sides are valid numbers, return true!
-        return firstTest && secondTest;
+        return checkRatioInputAgainstRegexes(val).isValid;
       },
     });
 
