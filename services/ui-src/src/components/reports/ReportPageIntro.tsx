89c5cacfa2271fddc0415579f12fdf36452b74ad
Casie Siekman <casiesiekman@coformaesiekman.lan>
2023-12-01 15:19:22 -0600

    update header alignment in specific initiative dashboard

--- services/ui-src/src/components/reports/ReportPageIntro.tsx
+++ services/ui-src/src/components/reports/ReportPageIntro.tsx
@@ -55,13 +55,14 @@ const sx = {
   subsectionHeading: {
     fontWeight: "normal",
     fontSize: "4xl",
+    marginTop: "0.5rem",
   },
   hintTextBox: {
     color: "#5B616B",
     paddingTop: "1.5rem",
   },
   infoTextBox: {
-    marginTop: "2rem",
+    marginTop: "1.5rem",
     h3: {
       marginBottom: "-0.75rem",
     },
