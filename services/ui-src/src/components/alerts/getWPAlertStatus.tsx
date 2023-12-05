2f4eef8aab6b5b478983b17e790a1d02458a2a36
Garrett Rabian <garrett@coforma.io>
2023-12-05 16:37:56 -0500

    remove asterisks and 'if applicable' from initiatives

--- services/ui-src/src/components/alerts/getWPAlertStatus.tsx
+++ services/ui-src/src/components/alerts/getWPAlertStatus.tsx
@@ -3,9 +3,9 @@ import { ReportShape, AnyObject } from "types";
 //setting up function calls using entityType as call
 export const checkInitiativeTopics = (fieldData: any, entities: any[]) => {
   let topics = [
-    "Transitions and transition coordination services*",
-    "Housing-related supports*",
-    "Quality measurement and improvement*",
+    "Transitions and transition coordination services",
+    "Housing-related supports",
+    "Quality measurement and improvement",
   ];
 
   //if user did not answer previous question, alert stays on
@@ -16,10 +16,10 @@ export const checkInitiativeTopics = (fieldData: any, entities: any[]) => {
     return true;
 
   if (fieldData?.instructions_selfDirectedInitiatives[0]?.value === "Yes")
-    topics.push("Self-direction (*if applicable)");
+    topics.push("Self-direction");
 
   if (fieldData?.instructions_tribalInitiatives[0]?.value === "Yes")
-    topics.push("Tribal Initiative (*if applicable)");
+    topics.push("Tribal Initiative");
 
   //filter down to the values of the radio selection
   const values = entities?.flatMap((entity) =>
@@ -28,7 +28,9 @@ export const checkInitiativeTopics = (fieldData: any, entities: any[]) => {
     })
   );
   //check if the values matches all the topics, if all topics is covered, return false
-  return !topics.every((topic) => values?.includes(topic));
+  return !topics.every((topic) =>
+    values.some((value) => value.includes(topic))
+  );
 };
 //store function calls here
 const alertStatusFunctions: AnyObject = {
