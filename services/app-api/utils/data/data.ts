9ae171e353b95f095f88e69527121e3f1cea1c5f
Nick Summers <19439679+ntsummers1@users.noreply.github.com>
2023-12-05 09:31:36 -0500

    [CMDCT-3087] - Ben's Beast Mode Edit (#259)
    Co-authored-by: benmartin-coforma <Ben.Martin@coforma.io>
    Co-authored-by: Nicholas Summers <nick@coforma.io>

--- services/app-api/utils/data/data.ts
+++ services/app-api/utils/data/data.ts
@@ -11,8 +11,8 @@ export const removeNotApplicablePopsFromInitiatives = (
   fieldData: AnyObject
 ) => {
   // Gather the data we need
-  const targetPopulations = fieldData?.targetPopulations;
-  const initiatives = fieldData?.initiative;
+  const targetPopulations = fieldData.targetPopulations;
+  const initiatives = fieldData.initiative;
 
   /*
    * If we can't find any data on targetPopulations or initiatives, we don't
@@ -25,40 +25,34 @@ export const removeNotApplicablePopsFromInitiatives = (
    * if this is applicable to the MFP demonstration. If none are found,
    * we don't need to do anything
    */
-  const notApplicablePopulations = targetPopulations.filter(
-    (population: AnyObject) => {
-      const isApplicable =
-        population?.transitionBenchmarks_applicableToMfpDemonstration?.[0]
-          ?.value;
-      return isApplicable === "No";
-    }
-  );
-  if (notApplicablePopulations.length === 0) return fieldData;
+  const isNotApplicable = (population: AnyObject) =>
+    population.transitionBenchmarks_applicableToMfpDemonstration?.[0]?.value ===
+    "No";
+
+  const getPopulationName = (population: AnyObject) =>
+    population.isRequired
+      ? population.transitionBenchmarks_targetPopulationName
+      : `Other: ${population.transitionBenchmarks_targetPopulationName}`;
+
+  const notApplicablePopulationNames = targetPopulations
+    .filter(isNotApplicable)
+    .map(getPopulationName);
+
+  if (notApplicablePopulationNames.length === 0) return fieldData;
 
   /*
    * Now knowing what target populations a user doesn't feel is applicable
    * to the MFP demonstration, we need to look through the initiatives
    * and remove it from the data
    */
-  const cleanedInitiatives = initiatives.map((initiative: AnyObject) => {
+  for (let initiative of initiatives) {
     initiative.defineInitiative_targetPopulations =
-      initiative?.defineInitiative_targetPopulations?.filter(
-        (initiativePopulation: AnyObject) => {
-          for (const population of notApplicablePopulations) {
-            const populationName = population?.isRequired
-              ? population?.transitionBenchmarks_targetPopulationName
-              : `Other: ${population?.transitionBenchmarks_targetPopulationName}`;
-            if (populationName == initiativePopulation?.value) {
-              return false;
-            }
-          }
-          return true;
-        }
+      initiative.defineInitiative_targetPopulations?.filter(
+        (initiativePopulation: AnyObject) =>
+          !notApplicablePopulationNames.includes(initiativePopulation.value)
       );
-    return initiative;
-  });
+  }
 
   // Now set and return the cleaned up data!
-  fieldData.initiative = cleanedInitiatives;
   return fieldData;
 };
