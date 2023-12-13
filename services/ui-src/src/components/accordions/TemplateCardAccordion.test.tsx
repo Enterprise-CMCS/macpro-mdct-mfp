25008d8e6becf90c087f5c1f1f46582d66c9895b
benmartin-coforma <126210497+benmartin-coforma@users.noreply.github.com>
2023-12-13 08:28:37 -0700

    Add some miscellaneous tests in ui-src (#287)

--- services/ui-src/src/components/accordions/TemplateCardAccordion.test.tsx
+++ services/ui-src/src/components/accordions/TemplateCardAccordion.test.tsx
@@ -6,25 +6,30 @@ import { RouterWrappedComponent } from "utils/testing/setupJest";
 // components
 import { TemplateCardAccordion } from "components";
 import verbiage from "verbiage/pages/home";
+import { AnyObject } from "types";
 
-const accordionComponent = (
-  <RouterWrappedComponent>
-    <TemplateCardAccordion verbiage={verbiage.cards.WP.accordion} />
-  </RouterWrappedComponent>
-);
+const accordionComponent = (mockProps?: AnyObject) => {
+  const props = {
+    verbiage: verbiage.cards.WP.accordion,
+    ...mockProps,
+  };
+  return (
+    <RouterWrappedComponent>
+      <TemplateCardAccordion {...props} />
+    </RouterWrappedComponent>
+  );
+};
 
 describe("Test TemplateCardAccordion", () => {
-  beforeEach(() => {
-    render(accordionComponent);
-  });
-
   test("Accordion is visible", () => {
+    render(accordionComponent());
     expect(
       screen.getByText(verbiage.cards.WP.accordion.buttonLabel)
     ).toBeVisible();
   });
 
   test("Accordion default closed state only shows the question", () => {
+    render(accordionComponent());
     expect(
       screen.getByText(verbiage.cards.WP.accordion.buttonLabel)
     ).toBeVisible();
@@ -34,6 +39,7 @@ describe("Test TemplateCardAccordion", () => {
   });
 
   test("Accordion should show answer on click", async () => {
+    render(accordionComponent());
     const accordionQuestion = screen.getByText(
       verbiage.cards.WP.accordion.buttonLabel
     );
@@ -45,11 +51,45 @@ describe("Test TemplateCardAccordion", () => {
     expect(accordionQuestion).toBeVisible();
     expect(screen.getByText(verbiage.cards.WP.accordion.text)).toBeVisible();
   });
+
+  test("Accordion should render a list when given one", async () => {
+    const mockProps = {
+      verbiage: {
+        buttonLabel: "expand",
+        list: ["item one", "item two", "item three"],
+      },
+    };
+
+    render(accordionComponent(mockProps));
+    const button = screen.getByText("expand");
+    await userEvent.click(button);
+
+    expect(screen.getByText("item one")).toBeVisible();
+    expect(screen.getByText("item two")).toBeVisible();
+    expect(screen.getByText("item three")).toBeVisible();
+  });
+
+  test("Accordion should render a table when given one", async () => {
+    const mockProps = {
+      verbiage: {
+        buttonLabel: "expand",
+        table: {
+          headRow: ["mock column header"],
+        },
+      },
+    };
+
+    render(accordionComponent(mockProps));
+    const button = screen.getByText("expand");
+    await userEvent.click(button);
+
+    expect(screen.getByText("mock column header")).toBeVisible();
+  });
 });
 
 describe("Test TemplateCardAccordion accessibility", () => {
   it("Should not have basic accessibility issues", async () => {
-    const { container } = render(accordionComponent);
+    const { container } = render(accordionComponent());
     const results = await axe(container);
     expect(results).toHaveNoViolations();
   });
