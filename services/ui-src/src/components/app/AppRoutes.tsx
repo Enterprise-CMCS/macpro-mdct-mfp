cff8a9cf16b6b49820c09a95b077bbb903434b87
Nathan Keeys <nathankeeys@coformaatekeeys.myfiosgateway.com>
2024-01-05 12:03:07 -0500

    test fix

--- services/ui-src/src/components/app/AppRoutes.tsx
+++ services/ui-src/src/components/app/AppRoutes.tsx
@@ -18,6 +18,10 @@ import { ScrollToTopComponent, useStore } from "utils";
 // types
 import { ReportRoute, ReportType } from "types";
 
+global.structuredClone = jest.fn((val) => {
+  return JSON.parse(JSON.stringify(val));
+});
+
 export const AppRoutes = () => {
   const { userIsAdmin } = useStore().user ?? {};
   const { report } = useStore();
