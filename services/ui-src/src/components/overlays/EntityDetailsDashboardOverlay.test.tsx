28c7fe5453482cd6c3697acf01e8472c3c240385
Jess Tran <jess.tran@coforma.io>
2024-01-23 07:35:08 -0800

    [CMDCT-3193] Update EntityDetailsDashboardOverlay tests (#392)

---
+++ services/ui-src/src/components/overlays/EntityDetailsDashboardOverlay.test.tsx
@@ -0,0 +1,60 @@
+import { render, screen } from "@testing-library/react";
+import { RouterWrappedComponent } from "../../utils/testing/mockRouter";
+import {
+  mockUseEntityStore,
+  mockEntityDetailsDashboardOverlayJson,
+  mockFormField,
+  mockVerbiageIntro,
+} from "../../utils/testing/setupJest";
+import { EntityDetailsDashboardOverlay } from "./EntityDetailsDashboardOverlay";
+import { useStore } from "utils";
+import { axe } from "jest-axe";
+
+jest.mock("utils/state/useStore");
+const mockedUseStore = useStore as jest.MockedFunction<typeof useStore>;
+mockedUseStore.mockReturnValue(mockUseEntityStore);
+
+const mockDashboard = {
+  id: "mock id",
+  fields: [mockFormField],
+  name: "mock dashboard",
+  pageType: "entityDetailsDashboardOverlay",
+  verbiage: {
+    intro: mockVerbiageIntro,
+  },
+};
+
+const entityDetailsDashboardOverlayComponent = (
+  <RouterWrappedComponent>
+    <EntityDetailsDashboardOverlay
+      route={mockEntityDetailsDashboardOverlayJson}
+      dashboard={mockDashboard}
+    />
+  </RouterWrappedComponent>
+);
+
+describe("Test EntityDetailsDashboardOverlay", () => {
+  test("EntityDetailsDashboardOverlay view renders", () => {
+    render(entityDetailsDashboardOverlayComponent);
+    // Check that the header rendered
+    expect(
+      screen.getByText(
+        mockEntityDetailsDashboardOverlayJson.verbiage.intro.section
+      )
+    ).toBeVisible();
+
+    expect(
+      screen.getByText(
+        mockEntityDetailsDashboardOverlayJson.verbiage.intro.subsection
+      )
+    ).toBeVisible();
+  });
+});
+
+describe("Test EntityDetailsDashboardOverlay accessibility", () => {
+  it("Should not have basic accessibility issues", async () => {
+    const { container } = render(entityDetailsDashboardOverlayComponent);
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+});
