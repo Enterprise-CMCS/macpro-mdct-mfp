70ff970dbab4c0d87424753d22d3e10a8a2b98fe
Ajaita Saini <ajaita.saini@coforma.io>
2024-01-30 11:15:03 -0500

    home changes-- SAR

--- services/ui-src/src/verbiage/pages/home.ts
+++ services/ui-src/src/verbiage/pages/home.ts
@@ -46,7 +46,7 @@ export default {
       },
       accordion: {
         buttonLabel: "When is the MFP SAR due?",
-        text: "The MFP Semi-Annual Progress Report will need to be reviewed and submitted twice a year, within 60 days following the end of each second and fourth calendar year quarter. For example, the SAR Reporting Period 1 represents January-June and MFP recipients must submit their data within 60 days following June 30. The recipient must submit the progress report through the final reporting period of the recipient's demonstration period of performance, even if the recipient has not operated for a complete reporting period.",
+        text: "The MFP Semi-Annual Progress Report will need to be reviewed and submitted twice a year, within 60 days following the end of each second and fourth calendar year quarter. For example, the SAR Reporting Period 1 represents January-June and MFP recipients must submit their data within 60 days following June 30. The recipient must submit the progress report through the final reporting period of the recipient's Demonstration period of performance, even if the recipient has not operated for a complete reporting period.",
       },
     },
   },
