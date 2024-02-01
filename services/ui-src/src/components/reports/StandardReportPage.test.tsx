2064b4bf47f1c3fad04320a40d38b3cc62c494ee
Nicholas Summers <nick@coforma.io>
2024-01-31 19:05:41 -0500

    Prettier

--- services/ui-src/src/components/reports/StandardReportPage.test.tsx
+++ services/ui-src/src/components/reports/StandardReportPage.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { axe } from "jest-axe";
 import userEvent from "@testing-library/user-event";
 // components
