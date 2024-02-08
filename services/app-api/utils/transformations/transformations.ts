de50ae38228b9b651ca6b3f80871c731deb0fc4f
benmartin-coforma <126210497+benmartin-coforma@users.noreply.github.com>
2024-02-08 11:39:15 -0700

    Use the user-supplied name for Other funding sources (#398)
    Co-authored-by: Casie Siekman <casiesiekman@Coforma-FJ9GWMHCGJ-Casie-Siekman.local>

--- services/app-api/utils/transformations/transformations.ts
+++ services/app-api/utils/transformations/transformations.ts
@@ -15,6 +15,7 @@ import {
   isUsableForTransforms,
   TargetPopulation,
   WorkPlanFieldDataForTransforms,
+  FundingSource,
 } from "../types";
 
 export const removeConditionalRoutes = <T extends ReportRoute>(
@@ -352,15 +353,23 @@ export const fundingSources = (
     },
   ];
 
-  let quarters =
+  const quarters =
     reportPeriod === 1 ? firstPeriodQuarters : secondPeriodQuarters;
+
+  const getFundingSourceName = (fundingSource: FundingSource) => {
+    const selectedOption = fundingSource.fundingSources_wpTopic[0].value;
+    if (selectedOption !== "Other, specify") {
+      return selectedOption;
+    }
+    return fundingSource.initiative_wp_otherTopic!;
+  };
 
   return initiativeToUse.fundingSources.flatMap((fundingSource) => [
     {
       id: `fundingSourcesHeader_${randomUUID()}`,
       type: "sectionHeader",
       props: {
-        content: `${fundingSource.fundingSources_wpTopic[0].value}`,
+        content: `${getFundingSourceName(fundingSource)}`,
       },
     },
     {
