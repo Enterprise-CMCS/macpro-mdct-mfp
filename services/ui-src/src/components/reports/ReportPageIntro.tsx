53ef5191d2cccf13732e242c4086a3aeb9d1b3c1
Casie Siekman <casiesiekman@coformaesiekman.lan>
2024-02-09 16:39:01 -0600

    add instruction according to objective progress page

--- services/ui-src/src/components/reports/ReportPageIntro.tsx
+++ services/ui-src/src/components/reports/ReportPageIntro.tsx
@@ -2,8 +2,8 @@
 import { Box, Heading } from "@chakra-ui/react";
 import { InstructionsAccordion } from "components";
 // utils
-import { parseCustomHtml, useStore } from "utils";
-import { AnyObject, ReportType } from "types";
+import { parseCustomHtml } from "utils";
+import { AnyObject } from "types";
 import { ReportPeriod } from "./ReportPeriod";
 
 export const ReportPageIntro = ({
@@ -16,7 +16,6 @@ export const ReportPageIntro = ({
   ...props
 }: Props) => {
   const { section, subsection, hint, info } = text;
-  const { report } = useStore() ?? {};
 
   return (
     <Box sx={sx.introBox} {...props}>
@@ -27,13 +26,18 @@ export const ReportPageIntro = ({
         {initiativeName ? initiativeName : subsection}
       </Heading>
       {hint && <Box sx={sx.hintTextBox}>{hint}</Box>}
-      {accordion && <InstructionsAccordion verbiage={accordion} />}
-      {stepType === "evaluationPlan" && report?.reportType === ReportType.SAR && (
+      {accordion && stepType !== "objectiveProgress" && (
+        <InstructionsAccordion verbiage={accordion} />
+      )}
+      {stepType === "objectiveProgress" && (
         <Heading as="h3" sx={sx.subTitle}>
           Objectives progress
         </Heading>
       )}
       {info && <Box sx={sx.infoTextBox}>{parseCustomHtml(info)}</Box>}
+      {accordion && stepType === "objectiveProgress" && (
+        <InstructionsAccordion verbiage={accordion} />
+      )}
       <ReportPeriod
         text={text}
         reportPeriod={reportPeriod}
