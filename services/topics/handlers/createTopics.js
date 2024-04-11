e1552a8bb2394d6befa289082e7d3130ef13d0b9
BearHanded <bcm3223@gmail.com>
2024-04-11 09:14:32 -0400

    add topic support

---
+++ services/topics/handlers/createTopics.js
@@ -0,0 +1,43 @@
+import * as topics from "../libs/topics-lib.js";
+import _ from "lodash";
+
+const condensedTopicList = [
+  {
+    // topics for the mfp service's connector
+    topicPrefix: "aws.mdct.mfp",
+    version: ".v0",
+    numPartitions: 1,
+    replicationFactor: 3,
+    topics: [".wp-reports", ".wp-form", ".sar-reports", ".sar-form"],
+  },
+];
+
+/**
+ * Handler triggered on deploy by the serverless js to create known topics in bigmac
+ * @param {*} event
+ * @param {*} _context
+ * @param {*} _callback
+ */
+exports.handler = async function (event, _context, _callback) {
+  console.log("Received event:", JSON.stringify(event, null, 2)); // eslint-disable-line no-console
+  var topicList = [];
+
+  // Generate the complete topic list from the condensed version above.
+  for (var element of condensedTopicList) {
+    topicList.push(
+      ..._.map(element.topics, (topic) => {
+        return {
+          topic: `${element.topicPrefix}${topic}${element.version}`,
+          numPartitions: element.numPartitions,
+          replicationFactor: element.replicationFactor,
+        };
+      })
+    );
+  }
+
+  await topics.createTopics(
+    process.env.brokerString,
+    process.env.topicNamespace,
+    topicList
+  );
+};
