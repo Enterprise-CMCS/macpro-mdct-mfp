46466edfb7f9bf2efe95739825136b9419b28880
Jess Eldredge <jess.eldredge@coforma.io>
2024-05-17 09:09:31 -0400

    dashboard title can be undefined

--- services/ui-src/src/components/reports/DynamicModalOverlayReportPage.tsx
+++ services/ui-src/src/components/reports/DynamicModalOverlayReportPage.tsx
@@ -63,7 +63,9 @@ export const DynamicModalOverlayReportPage = ({
   const showAlert =
     report && errorMessage ? getWPAlertStatus(report, entityType) : false;
 
-  const dashTitle = `${verbiage.dashboardTitle}`;
+  const dashTitle = verbiage.dashboardTitle
+    ? `${verbiage.dashboardTitle}`
+    : null;
   const dashSubTitle = parseCustomHtml(
     (verbiage as AnyObject)?.dashboardSubtitle
   );
