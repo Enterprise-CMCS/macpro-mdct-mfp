1480a01889780c6fb64917ee08eb7892bcc79a01
Jess Eldredge <jess.eldredge@coforma.io>
2024-06-06 10:32:58 -0400

    More smol config

--- playwright.config.ts
+++ playwright.config.ts
@@ -38,16 +38,6 @@ export default defineConfig({
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
-
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
-
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
   ],
 
   /* Run your local dev server before starting the tests */
@@ -55,5 +45,6 @@ export default defineConfig({
     command: "./run local",
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
+    stdout: "pipe",
   },
 });
