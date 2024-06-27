ad0a3e1cf4ed9ebafd9bb84b941ce0e526170733
Bangbay Siboliban <bangbay@bluetiger.digital>
2024-06-27 11:36:22 -0700

    Use testA11y

--- services/ui-src/src/components/statusing/EditButton.test.tsx
+++ services/ui-src/src/components/statusing/EditButton.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import { axe } from "jest-axe";
+import { testA11y } from "utils/testing/commonTests";
 import { RouterWrappedComponent } from "utils/testing/setupJest";
 import { EditButton } from "./EditButton";
 
@@ -25,28 +25,24 @@ const EditButtonComponent = (
   </RouterWrappedComponent>
 );
 
-describe("EditButton", () => {
-  test("EditButton when not editable", () => {
-    render(EditButtonComponentNotEditable);
-    const viewButton = screen.getByRole("button");
-    const viewIcon = screen.getByRole("img");
-    expect(viewButton).toHaveTextContent("View");
-    expect(viewIcon).toHaveAttribute("alt", "View Program");
+describe("<EditButton />", () => {
+  describe("EditButton", () => {
+    test("EditButton when not editable", () => {
+      render(EditButtonComponentNotEditable);
+      const viewButton = screen.getByRole("button");
+      const viewIcon = screen.getByRole("img");
+      expect(viewButton).toHaveTextContent("View");
+      expect(viewIcon).toHaveAttribute("alt", "View Program");
+    });
+
+    test("EditButton when editable", () => {
+      render(EditButtonComponent);
+      const editButton = screen.getByRole("button");
+      const editIcon = screen.getByRole("img");
+      expect(editButton).toHaveTextContent("Edit");
+      expect(editIcon).toHaveAttribute("alt", "Edit Program");
+    });
   });
 
-  test("EditButton when editable", () => {
-    render(EditButtonComponent);
-    const editButton = screen.getByRole("button");
-    const editIcon = screen.getByRole("img");
-    expect(editButton).toHaveTextContent("Edit");
-    expect(editIcon).toHaveAttribute("alt", "Edit Program");
-  });
-});
-
-describe("Test EditButton accessibility", () => {
-  test("Should not have basic accessibility issues", async () => {
-    const { container } = render(EditButtonComponent);
-    const results = await axe(container);
-    expect(results).toHaveNoViolations();
-  });
+  testA11y(EditButtonComponent);
 });
