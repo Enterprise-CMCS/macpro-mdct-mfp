1111e9da4d5fbd5fee73f7482ed271d492ef16d2
Jess Eldredge <jess.eldredge@coforma.io>
2024-06-28 13:51:43 -0400

    Organize theme styles into separate files

---
+++ services/ui-src/src/styles/components/button.ts
@@ -0,0 +1,106 @@
+import { ComponentStyleConfig } from "@chakra-ui/react";
+
+const baseStyles = {
+  transition: "all 0.3s ease",
+  borderRadius: "0.25rem",
+  px: "1.5em",
+  py: "0.5em",
+  fontWeight: "bold",
+  width: "fit-content",
+  "&:disabled, &:disabled:hover": {
+    color: "palette.gray",
+    backgroundColor: "palette.gray_lighter",
+  },
+};
+
+const primaryVariant = {
+  backgroundColor: "palette.primary",
+  color: "palette.white",
+  "&:hover": {
+    backgroundColor: "palette.primary_darker",
+  },
+  "&:visited, &:visited:hover": {
+    backgroundColor: "palette.primary",
+    color: "palette.white",
+  },
+  "&:disabled, &:disabled:hover": {
+    color: "palette.gray",
+    backgroundColor: "palette.gray_lighter",
+  },
+};
+
+const outlineVariant = {
+  backgroundColor: "transparent",
+  border: "1px solid",
+  borderColor: "palette.primary",
+  color: "palette.primary",
+  textDecoration: "none",
+  "&:hover": {
+    backgroundColor: "transparent",
+    color: "palette.primary_darker",
+  },
+  "&:visited, &:visited:hover": {
+    backgroundColor: "transparent",
+    color: "palette.primary",
+  },
+  "&:disabled, &:disabled:hover": {
+    color: "palette.gray_lighter",
+    borderColor: "palette.gray_lighter",
+  },
+};
+
+const transparentVariant = {
+  backgroundColor: "transparent",
+  color: "palette.primary",
+  textDecoration: "underline",
+  padding: "0",
+  "&:hover": {
+    backgroundColor: "transparent",
+    color: "palette.primary_darker",
+  },
+  "&:visited, &:visited:hover": {
+    backgroundColor: "transparent",
+    color: "palette.primary",
+  },
+  "&:disabled, &:disabled:hover": {
+    color: "palette.gray_lighter",
+  },
+};
+
+const variants = {
+  primary: primaryVariant,
+  outline: outlineVariant,
+  transparent: transparentVariant,
+};
+
+const sizes = {
+  sm: {
+    fontSize: "sm",
+    fontWeight: "400",
+    px: "0.5em",
+    py: "0.25em",
+  },
+  md: {
+    fontSize: "md",
+    fontWeight: "700",
+    px: "1.5em",
+    py: "0.5em",
+  },
+  lg: {
+    fontSize: "lg",
+    px: "1.5em",
+    py: "1em",
+  },
+};
+
+const buttonTheme: ComponentStyleConfig = {
+  baseStyle: baseStyles,
+  sizes: sizes,
+  variants: variants,
+  defaultProps: {
+    variant: "primary",
+    size: "md",
+  },
+};
+
+export default buttonTheme;
---
+++ services/ui-src/src/styles/foundations/filters.ts
@@ -0,0 +1,10 @@
+export const svgFilters = {
+  primary:
+    "brightness(0) saturate(100%) invert(30%) sepia(93%) saturate(1282%) hue-rotate(181deg) brightness(91%) contrast(101%)",
+  primary_darker:
+    "brightness(0) saturate(100%) invert(19%) sepia(43%) saturate(3547%) hue-rotate(185deg) brightness(97%) contrast(101%)",
+  white:
+    "brightness(0) saturate(100%) invert(100%) sepia(0%) saturate(7500%) hue-rotate(142deg) brightness(115%) contrast(115%);",
+  gray_lighter:
+    "brightness(0) saturate(100%) invert(91%) sepia(0%) saturate(89%) hue-rotate(162deg) brightness(97%) contrast(93%);",
+};
