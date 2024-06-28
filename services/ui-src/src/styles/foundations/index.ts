2d39c4d06381c51caa5f6c4c9bcafc38ebb6ca6f
Jess Eldredge <jess.eldredge@coforma.io>
2024-06-28 13:51:43 -0400

    Organize theme styles into separate files

---
+++ services/ui-src/src/styles/foundations/index.ts
@@ -0,0 +1,9 @@
+import { breakpoints } from "./breakpoints";
+import { fonts } from "./fonts";
+import { lineHeights } from "./lineHeights";
+
+export const foundations = {
+  breakpoints: breakpoints,
+  fonts: fonts,
+  lineHeights: lineHeights,
+};
