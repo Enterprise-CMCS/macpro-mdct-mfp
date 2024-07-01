578d8fbc9289574338470b219093076880b8987e
Jess Eldredge <jess.eldredge@coforma.io>
2024-07-01 08:30:57 -0400

    More updates to index

--- services/ui-src/src/styles/theme.ts
+++ services/ui-src/src/styles/theme.ts
@@ -5,10 +5,7 @@ import { extendTheme } from "@chakra-ui/react";
 import { styles } from "./styles";
 
 // Foundational style overrides
-import { breakpoints } from "./foundations/breakpoints";
-import { lineHeights } from "./foundations/lineHeights";
-import { fonts } from "./foundations/fonts";
-import { colors } from "./foundations/colors";
+import { foundations } from "./foundations";
 
 // Component style overrides
 import { components } from "./components";
@@ -16,26 +13,7 @@ import { components } from "./components";
 // Component style overrides
 
 const theme = extendTheme({
-  sizes: {
-    appMax: "100vw",
-    basicPageWidth: "46rem",
-    reportPageWidth: "46rem",
-    // font sizes: https://design.cms.gov/utilities/font-size/
-    xs: "0.75rem", // 12px
-    sm: "0.875rem", // 14px
-    md: "1rem", // 16px
-    lg: "1.125rem", // 18px
-    xl: "1.3125rem", // 21px
-    "2xl": "1.5rem", // 24px
-    "3xl": "1.875rem", // 30px
-    "4xl": "2.25rem", // 36px
-    "5xl": "3rem", // 48px
-    "6xl": "3.75rem", // 60px
-  },
-  breakpoints,
-  fonts,
-  lineHeights,
-  colors,
+  foundations,
   components,
   styles,
 });
