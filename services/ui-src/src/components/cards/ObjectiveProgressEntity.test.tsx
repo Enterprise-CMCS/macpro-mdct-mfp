d2f57ccc13e92d7b11475d0537a576a7c2b7206e
Britt <britt.bujacich@gmail.com>
2024-07-12 08:48:45 -0600

    refactor EntityCardBottomSection cmdct-3359 (#680)

---
+++ services/ui-src/src/components/cards/ObjectiveProgressEntity.test.tsx
@@ -0,0 +1,40 @@
+import { render, screen } from "@testing-library/react";
+import { mockObjectiveProgressFormattedEntityData } from "utils/testing/setupJest";
+import { ObjectiveProgressEntity } from "./ObjectiveProgressEntity";
+import { testA11y } from "utils/testing/commonTests";
+
+const objectiveProgressEntityComponent = (
+  <ObjectiveProgressEntity
+    formattedEntityData={mockObjectiveProgressFormattedEntityData}
+    entityCompleted={true}
+  />
+);
+const renderedText = [
+  `${mockObjectiveProgressFormattedEntityData.quarterProjections[0].id} Target:`,
+  `${mockObjectiveProgressFormattedEntityData.quarterProjections[1].id} Target:`,
+  `${mockObjectiveProgressFormattedEntityData.quarterActuals[0].id} Actual:`,
+  `${mockObjectiveProgressFormattedEntityData.quarterActuals[1].id} Actual:`,
+  mockObjectiveProgressFormattedEntityData.missedTargetReason,
+  mockObjectiveProgressFormattedEntityData.performanceMeasureProgress,
+  mockObjectiveProgressFormattedEntityData.performanceMeasureProgress,
+];
+
+describe("<ObjectiveProgressEntity />", () => {
+  test("ObjectiveProgressEntity renders correctly", () => {
+    render(objectiveProgressEntityComponent);
+    expect(
+      screen.getByRole("heading", { name: "mockObjectiveName" })
+    ).toBeVisible();
+
+    renderedText.forEach((text) =>
+      expect(screen.getByText(text)).toBeVisible()
+    );
+  });
+
+  testA11y(
+    <ObjectiveProgressEntity
+      formattedEntityData={mockObjectiveProgressFormattedEntityData}
+      entityCompleted={true}
+    />
+  );
+});
