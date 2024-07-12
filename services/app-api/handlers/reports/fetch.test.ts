71bac7eb392441e20cc35357f1389ec274eb142e
Bangbay Siboliban <bangbay@bluetiger.digital>
2024-07-12 09:30:56 -0700

    Refactor duplicate code

--- services/app-api/handlers/reports/fetch.test.ts
+++ services/app-api/handlers/reports/fetch.test.ts
@@ -56,7 +56,7 @@ let consoleSpy: {
   warn: jest.fn() as jest.SpyInstance,
 };
 
-describe("Test fetchReport API method", () => {
+describe("handlers/reports/fetch", () => {
   beforeEach(() => {
     jest.restoreAllMocks();
     mockAuthUtil.isAuthorizedToFetchState.mockReturnValueOnce(true);
@@ -64,131 +64,126 @@ describe("Test fetchReport API method", () => {
     consoleSpy.warn = jest.spyOn(console, "warn").mockImplementation();
   });
 
-  test("Test Report not found in DynamoDB", async () => {
-    (getReportMetadata as jest.Mock).mockResolvedValue(undefined);
-    const res = await fetchReport(testReadEvent, null);
-    expect(consoleSpy.debug).toHaveBeenCalled();
-    expect(res.statusCode).toBe(StatusCodes.NOT_FOUND);
-  });
-
-  test("Test Report Form not found in S3", async () => {
-    (getReportMetadata as jest.Mock).mockResolvedValue(mockDynamoData);
-    (getReportFormTemplate as jest.Mock).mockResolvedValue(undefined);
-    (getReportFieldData as jest.Mock).mockResolvedValue(mockReportFieldData);
-    const res = await fetchReport(testReadEvent, null);
-    expect(consoleSpy.debug).toHaveBeenCalled();
-    expect(res.statusCode).toBe(StatusCodes.NOT_FOUND);
-  });
-
-  test("Test Field Data not found in S3", async () => {
-    (getReportMetadata as jest.Mock).mockResolvedValue(mockDynamoData);
-    (getReportFormTemplate as jest.Mock).mockResolvedValue(mockReportJson);
-    (getReportFieldData as jest.Mock).mockResolvedValue(undefined);
-    const res = await fetchReport(testReadEvent, null);
-    expect(consoleSpy.debug).toHaveBeenCalled();
-    expect(res.statusCode).toBe(StatusCodes.NOT_FOUND);
-  });
-
-  test("Test Successful Report Fetch w/ Incomplete Report", async () => {
-    (getReportMetadata as jest.Mock).mockResolvedValue(mockDynamoData);
-    (getReportFormTemplate as jest.Mock).mockResolvedValue(mockReportJson);
-    (getReportFieldData as jest.Mock).mockResolvedValue(mockReportFieldData);
-    const res = await fetchReport(testReadEvent, null);
-    expect(consoleSpy.debug).toHaveBeenCalled();
-    expect(res.statusCode).toBe(StatusCodes.SUCCESS);
-    const body = JSON.parse(res.body);
-    expect(body.lastAlteredBy).toContain("Thelonious States");
-    expect(body.submissionName).toContain("testProgram");
-    expect(body.completionStatus).toMatchObject(
-      mockDynamoData.completionStatus
-    );
-    expect(body.isComplete).toStrictEqual(false);
-    expect(body.fieldData).toStrictEqual(mockReportFieldData);
-    expect(body.formTemplate).toStrictEqual(mockReportJson);
-  });
-
-  test("Test Successful Report Fetch w/ Complete Report", async () => {
-    (getReportMetadata as jest.Mock).mockResolvedValue(
-      mockDynamoDataWPCompleted
-    );
-    (getReportFormTemplate as jest.Mock).mockResolvedValue(mockReportJson);
-    (getReportFieldData as jest.Mock).mockResolvedValue(mockReportFieldData);
-    const res = await fetchReport(testReadEvent, null);
-    expect(consoleSpy.debug).toHaveBeenCalled();
-    expect(res.statusCode).toBe(StatusCodes.SUCCESS);
-    const body = JSON.parse(res.body);
-    expect(body.lastAlteredBy).toContain("Thelonious States");
-    expect(body.submissionName).toContain("testProgram");
-    expect(body.completionStatus).toMatchObject({
-      "step-one": true,
+  describe("Test fetchReport API method", () => {
+    test("Test Report not found in DynamoDB", async () => {
+      (getReportMetadata as jest.Mock).mockResolvedValue(undefined);
+      const res = await fetchReport(testReadEvent, null);
+      expect(consoleSpy.debug).toHaveBeenCalled();
+      expect(res.statusCode).toBe(StatusCodes.NOT_FOUND);
     });
-    expect(body.isComplete).toStrictEqual(true);
-    expect(body.fieldData).toStrictEqual(mockReportFieldData);
-    expect(body.formTemplate).toStrictEqual(mockReportJson);
+
+    test("Test Report Form not found in S3", async () => {
+      (getReportMetadata as jest.Mock).mockResolvedValue(mockDynamoData);
+      (getReportFormTemplate as jest.Mock).mockResolvedValue(undefined);
+      (getReportFieldData as jest.Mock).mockResolvedValue(mockReportFieldData);
+      const res = await fetchReport(testReadEvent, null);
+      expect(consoleSpy.debug).toHaveBeenCalled();
+      expect(res.statusCode).toBe(StatusCodes.NOT_FOUND);
+    });
+
+    test("Test Field Data not found in S3", async () => {
+      (getReportMetadata as jest.Mock).mockResolvedValue(mockDynamoData);
+      (getReportFormTemplate as jest.Mock).mockResolvedValue(mockReportJson);
+      (getReportFieldData as jest.Mock).mockResolvedValue(undefined);
+      const res = await fetchReport(testReadEvent, null);
+      expect(consoleSpy.debug).toHaveBeenCalled();
+      expect(res.statusCode).toBe(StatusCodes.NOT_FOUND);
+    });
+
+    test("Test Successful Report Fetch w/ Incomplete Report", async () => {
+      (getReportMetadata as jest.Mock).mockResolvedValue(mockDynamoData);
+      (getReportFormTemplate as jest.Mock).mockResolvedValue(mockReportJson);
+      (getReportFieldData as jest.Mock).mockResolvedValue(mockReportFieldData);
+      const res = await fetchReport(testReadEvent, null);
+      expect(consoleSpy.debug).toHaveBeenCalled();
+      expect(res.statusCode).toBe(StatusCodes.SUCCESS);
+      const body = JSON.parse(res.body);
+      expect(body.lastAlteredBy).toContain("Thelonious States");
+      expect(body.submissionName).toContain("testProgram");
+      expect(body.completionStatus).toMatchObject(
+        mockDynamoData.completionStatus
+      );
+      expect(body.isComplete).toStrictEqual(false);
+      expect(body.fieldData).toStrictEqual(mockReportFieldData);
+      expect(body.formTemplate).toStrictEqual(mockReportJson);
+    });
+
+    test("Test Successful Report Fetch w/ Complete Report", async () => {
+      (getReportMetadata as jest.Mock).mockResolvedValue(
+        mockDynamoDataWPCompleted
+      );
+      (getReportFormTemplate as jest.Mock).mockResolvedValue(mockReportJson);
+      (getReportFieldData as jest.Mock).mockResolvedValue(mockReportFieldData);
+      const res = await fetchReport(testReadEvent, null);
+      expect(consoleSpy.debug).toHaveBeenCalled();
+      expect(res.statusCode).toBe(StatusCodes.SUCCESS);
+      const body = JSON.parse(res.body);
+      expect(body.lastAlteredBy).toContain("Thelonious States");
+      expect(body.submissionName).toContain("testProgram");
+      expect(body.completionStatus).toMatchObject({
+        "step-one": true,
+      });
+      expect(body.isComplete).toStrictEqual(true);
+      expect(body.fieldData).toStrictEqual(mockReportFieldData);
+      expect(body.formTemplate).toStrictEqual(mockReportJson);
+    });
+
+    test("Test reportKeys not provided throws 400 error", async () => {
+      const noKeyEvent: APIGatewayProxyEvent = {
+        ...testReadEvent,
+        pathParameters: {},
+      };
+      const res = await fetchReport(noKeyEvent, null);
+      expect(consoleSpy.warn).toHaveBeenCalled();
+      expect(res.statusCode).toBe(400);
+      expect(res.body).toContain(error.NO_KEY);
+    });
+
+    test("Test reportKeys empty throws 400 error", async () => {
+      const noKeyEvent: APIGatewayProxyEvent = {
+        ...testReadEvent,
+        pathParameters: { state: "", id: "" },
+      };
+      const res = await fetchReport(noKeyEvent, null);
+      expect(consoleSpy.warn).toHaveBeenCalled();
+      expect(res.statusCode).toBe(400);
+      expect(res.body).toContain(error.NO_KEY);
+    });
   });
 
-  test("Test reportKeys not provided throws 400 error", async () => {
-    const noKeyEvent: APIGatewayProxyEvent = {
-      ...testReadEvent,
-      pathParameters: {},
-    };
-    const res = await fetchReport(noKeyEvent, null);
-    expect(consoleSpy.warn).toHaveBeenCalled();
-    expect(res.statusCode).toBe(400);
-    expect(res.body).toContain(error.NO_KEY);
-  });
+  describe("Test fetchReportsByState API method", () => {
+    test("Test successful call", async () => {
+      (queryReportMetadatasForState as jest.Mock).mockResolvedValueOnce([
+        mockDynamoData,
+      ]);
+      const res = await fetchReportsByState(testReadEventByState, null);
+      expect(consoleSpy.debug).toHaveBeenCalled();
+      expect(res.statusCode).toBe(StatusCodes.SUCCESS);
+      const body = JSON.parse(res.body);
+      expect(body[0].lastAlteredBy).toContain("Thelonious States");
+      expect(body[0].submissionName).toContain("testProgram");
+    });
 
-  test("Test reportKeys empty throws 400 error", async () => {
-    const noKeyEvent: APIGatewayProxyEvent = {
-      ...testReadEvent,
-      pathParameters: { state: "", id: "" },
-    };
-    const res = await fetchReport(noKeyEvent, null);
-    expect(consoleSpy.warn).toHaveBeenCalled();
-    expect(res.statusCode).toBe(400);
-    expect(res.body).toContain(error.NO_KEY);
-  });
-});
+    test("Test reportKeys not provided throws 400 error", async () => {
+      const noKeyEvent: APIGatewayProxyEvent = {
+        ...testReadEventByState,
+        pathParameters: {},
+      };
+      const res = await fetchReportsByState(noKeyEvent, null);
+      expect(consoleSpy.warn).toHaveBeenCalled();
+      expect(res.statusCode).toBe(400);
+      expect(res.body).toContain(error.NO_KEY);
+    });
 
-describe("Test fetchReportsByState API method", () => {
-  beforeEach(() => {
-    jest.restoreAllMocks();
-    mockAuthUtil.isAuthorizedToFetchState.mockReturnValueOnce(true);
-    consoleSpy.debug = jest.spyOn(console, "debug").mockImplementation();
-    consoleSpy.warn = jest.spyOn(console, "warn").mockImplementation();
-  });
-
-  test("Test successful call", async () => {
-    (queryReportMetadatasForState as jest.Mock).mockResolvedValueOnce([
-      mockDynamoData,
-    ]);
-    const res = await fetchReportsByState(testReadEventByState, null);
-    expect(consoleSpy.debug).toHaveBeenCalled();
-    expect(res.statusCode).toBe(StatusCodes.SUCCESS);
-    const body = JSON.parse(res.body);
-    expect(body[0].lastAlteredBy).toContain("Thelonious States");
-    expect(body[0].submissionName).toContain("testProgram");
-  });
-
-  test("Test reportKeys not provided throws 400 error", async () => {
-    const noKeyEvent: APIGatewayProxyEvent = {
-      ...testReadEventByState,
-      pathParameters: {},
-    };
-    const res = await fetchReportsByState(noKeyEvent, null);
-    expect(consoleSpy.warn).toHaveBeenCalled();
-    expect(res.statusCode).toBe(400);
-    expect(res.body).toContain(error.NO_KEY);
-  });
-
-  test("Test reportKeys empty throws 400 error", async () => {
-    const noKeyEvent: APIGatewayProxyEvent = {
-      ...testReadEventByState,
-      pathParameters: { state: "" },
-    };
-    const res = await fetchReportsByState(noKeyEvent, null);
-    expect(consoleSpy.warn).toHaveBeenCalled();
-    expect(res.statusCode).toBe(400);
-    expect(res.body).toContain(error.NO_KEY);
+    test("Test reportKeys empty throws 400 error", async () => {
+      const noKeyEvent: APIGatewayProxyEvent = {
+        ...testReadEventByState,
+        pathParameters: { state: "" },
+      };
+      const res = await fetchReportsByState(noKeyEvent, null);
+      expect(consoleSpy.warn).toHaveBeenCalled();
+      expect(res.statusCode).toBe(400);
+      expect(res.body).toContain(error.NO_KEY);
+    });
   });
 });
