bbd293348f93e1d20628aa3399bce8c7c27fe3f0
Jess Eldredge <jess.eldredge@coforma.io>
2024-08-07 15:49:53 -0400

    Create fixtures and pages for home page tests

--- tests/e2e/home.spec.ts
+++ tests/e2e/home.spec.ts
@@ -1,25 +1,19 @@
-import { test, expect } from "@playwright/test";
-import { logInStateUser, logInAdminUser } from "./helpers";
+import { test } from "./fixtures/base";
 
-test("Should see the correct home page as a state user", async ({ page }) => {
-  await page.goto("/");
-  await logInStateUser({ page });
-
-  await expect(
-    page.getByRole("button", { name: "Enter Work Plan online" })
-  ).toBeVisible();
-  await expect(
-    page.getByRole("button", { name: "Enter SAR online" })
-  ).toBeVisible();
+test("Should see the correct home page as a state user", async ({
+  loginPage,
+  stateHomePage,
+}) => {
+  await loginPage.goto();
+  await loginPage.loginStateUser();
+  await stateHomePage.isReady();
 });
 
-test("Should see the correct home page as an admin user", async ({ page }) => {
-  await page.goto("/");
-  await logInAdminUser({ page });
-
-  await expect(
-    page.getByRole("combobox", {
-      name: "List of states, including District of Columbia and Puerto Rico",
-    })
-  ).toBeVisible();
+test("Should see the correct home page as an admin user", async ({
+  loginPage,
+  adminHomePage,
+}) => {
+  await loginPage.goto();
+  await loginPage.loginAdminUser();
+  await adminHomePage.isReady();
 });
