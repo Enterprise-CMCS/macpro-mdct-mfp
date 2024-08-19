e7eab665a3ffe0e128353766f492a928902f520a
Britt <britt.bujacich@gmail.com>
2024-08-19 11:50:47 -0600

    cmdct-3881 - changes to archive, remove unarchive (#730)

--- tests/e2e/helpers.ts
+++ tests/e2e/helpers.ts
@@ -8,6 +8,7 @@ export const statePassword = process.env.SEED_STATE_USER_PASSWORD!;
 export const stateAbbreviation = process.env.SEED_STATE || "PR";
 export const stateName = process.env.SEED_STATE_NAME || "Puerto Rico";
 export const firstPeriod: number = 1;
+export const secondPeriod: number = 2;
 
 async function logInUser(page: Page, email: string, password: string) {
   await page.goto("/");
@@ -51,11 +52,19 @@ async function archiveReports(page: Page) {
     .all();
   if (archiveButtons.length > 0) {
     await archiveButtons[0].click();
+    const modal = page.getByRole("dialog");
+    await expect(modal).toBeVisible();
+    await expect(modal).toContainText(
+      "Are you sure you want to archive this MFP Work Plan?"
+    );
+    await modal.getByRole("textbox").fill("ARCHIVE");
+    await modal.getByRole("button", { name: "Archive" }).click();
     await page.waitForResponse(
       (response: Response) =>
         response.url().includes(`reports/archive/WP/${stateAbbreviation}/`) &&
         response.status() == 200
     );
+    await expect(modal).not.toBeVisible();
     await expect(page.getByRole("table")).toBeVisible();
     await archiveReports(page);
   }
@@ -69,6 +78,7 @@ export async function archiveExistingWPs(page: Page) {
     })
     .selectOption(stateAbbreviation);
   await page.getByLabel("MFP Work Plan").click();
+
   await page.getByRole("button", { name: "Go to Report Dashboard" }).click();
   await expect(page).toHaveURL("/wp");
   await expect(page.getByRole("table")).toBeVisible();
