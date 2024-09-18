8894e732a1478527d189608bcbbf642de54ab58e
benmartin-coforma <Ben.Martin@coforma.io>
2024-09-18 09:02:12 -0600

    A few more lines of test coverage

--- services/app-api/handlers/reports/fetch.test.ts
+++ services/app-api/handlers/reports/fetch.test.ts
@@ -17,6 +17,7 @@ import {
 // types
 import { APIGatewayProxyEvent } from "../../utils/types";
 import { StatusCodes } from "../../utils/responses/response-lib";
+import { isAuthorizedToFetchState } from "../../utils/auth/authorization";
 
 jest.mock("../../storage/reports", () => ({
   getReportFieldData: jest.fn(),
@@ -28,10 +29,8 @@ jest.mock("../../storage/reports", () => ({
 jest.mock("../../utils/auth/authorization", () => ({
   hasPermissions: jest.fn(() => {}),
   isAuthenticated: jest.fn().mockReturnValue(true),
-  isAuthorizedToFetchState: jest.fn(() => {}),
+  isAuthorizedToFetchState: jest.fn().mockReturnValue(true),
 }));
-
-const mockAuthUtil = require("../../utils/auth/authorization");
 
 const testReadEvent: APIGatewayProxyEvent = {
   ...proxyEvent,
@@ -59,8 +58,7 @@ let consoleSpy: {
 
 describe("handlers/reports/fetch", () => {
   beforeEach(() => {
-    jest.restoreAllMocks();
-    mockAuthUtil.isAuthorizedToFetchState.mockReturnValueOnce(true);
+    jest.clearAllMocks();
     consoleSpy.debug = jest.spyOn(console, "debug").mockImplementation();
     consoleSpy.warn = jest.spyOn(console, "warn").mockImplementation();
   });
@@ -129,6 +127,26 @@ describe("handlers/reports/fetch", () => {
       expect(body.formTemplate).toStrictEqual(mockReportJson);
     });
 
+    test("Test Successful Report Fetch, creating completionStatus", async () => {
+      const metadataWithNoCompletionStatus = {
+        ...mockDynamoDataWPCompleted,
+        completionStatus: undefined,
+      };
+      (getReportMetadata as jest.Mock).mockResolvedValue(
+        metadataWithNoCompletionStatus
+      );
+      (getReportFormTemplate as jest.Mock).mockResolvedValue(mockReportJson);
+      (getReportFieldData as jest.Mock).mockResolvedValue(mockReportFieldData);
+      const res = await fetchReport(testReadEvent, null);
+      expect(res.statusCode).toBe(StatusCodes.Ok);
+      const body = JSON.parse(res.body!);
+      expect(body.completionStatus).toEqual({
+        "/mock/mock-route-1": false,
+        "/mock/mock-route-2": {},
+      });
+      expect(body.isComplete).toEqual(false);
+    });
+
     test("Test reportKeys not provided throws 400 error", async () => {
       const noKeyEvent: APIGatewayProxyEvent = {
         ...testReadEvent,
@@ -149,6 +167,13 @@ describe("handlers/reports/fetch", () => {
       expect(consoleSpy.warn).toHaveBeenCalled();
       expect(res.statusCode).toBe(StatusCodes.BadRequest);
       expect(res.body).toContain(error.NO_KEY);
+    });
+
+    test("Test unauthorized returns 403", async () => {
+      (isAuthorizedToFetchState as jest.Mock).mockReturnValueOnce(false);
+      const res = await fetchReport(testReadEvent, null);
+      expect(res.statusCode).toBe(StatusCodes.Forbidden);
+      expect(res.body).toContain(error.UNAUTHORIZED);
     });
   });
 
@@ -185,6 +210,13 @@ describe("handlers/reports/fetch", () => {
       expect(consoleSpy.warn).toHaveBeenCalled();
       expect(res.statusCode).toBe(StatusCodes.BadRequest);
       expect(res.body).toContain(error.NO_KEY);
+    });
+
+    test("Test unauthorized returns 403", async () => {
+      (isAuthorizedToFetchState as jest.Mock).mockReturnValueOnce(false);
+      const res = await fetchReportsByState(testReadEventByState, null);
+      expect(res.statusCode).toBe(StatusCodes.Forbidden);
+      expect(res.body).toContain(error.UNAUTHORIZED);
     });
   });
 });
