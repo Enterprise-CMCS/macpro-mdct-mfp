94b0b08669ee86dff662fdf971c61cf412f79bfa
Jessica Eldredge <jess.eldredge@coforma.io>
2024-10-01 09:28:25 -0400

    Playwright test for creating and submitting a WP (#759)

--- tests/e2e/wp/create.spec.ts
+++ tests/e2e/wp/create.spec.ts
@@ -1,52 +1,150 @@
 import { archiveExistingWPs, logInStateUser } from "../utils";
 import { test, expect } from "../utils/fixtures/base";
+import { WPInitiativeOverlayPage } from "../utils/pageObjects/wp/wpInitiativeOverlay.page";
 
-test("State user can create a Work Plan", async ({
-  page,
-  stateHomePage,
-  wpDashboard,
-}) => {
-  await archiveExistingWPs(page);
-  await logInStateUser(page);
+test.describe("Creating a new Work Plan", () => {
+  test("State user can create a Work Plan", async ({
+    page,
+    stateHomePage,
+    wpDashboard,
+  }) => {
+    await archiveExistingWPs(page);
+    await logInStateUser(page);
 
-  // View WPs
-  await stateHomePage.wpButton.click();
-  await wpDashboard.isReady();
+    // View WPs
+    await stateHomePage.wpButton.click();
+    await wpDashboard.isReady();
 
-  // check if work plans exist already or not
-  if (await wpDashboard.createButton.isVisible()) {
-    await wpDashboard.createButton.click();
-    await expect(wpDashboard.modal).toBeVisible();
-    await expect(wpDashboard.modal).toContainText("Add new MFP Work Plan");
+    // check if work plans exist already or not
+    if (await wpDashboard.createButton.isVisible()) {
+      await wpDashboard.createButton.click();
+      await expect(wpDashboard.modal).toBeVisible();
+      await expect(wpDashboard.modal).toContainText("Add new MFP Work Plan");
 
-    await wpDashboard.createNewWP();
+      await wpDashboard.createNewWP();
 
-    // Confirm created WP is in table
-    await wpDashboard.getReports();
-    await expect(wpDashboard.firstReport).toBeVisible();
+      // Confirm created WP is in table
+      await wpDashboard.getReports();
+      await expect(wpDashboard.firstReport).toBeVisible();
+      const editNewWPButton = wpDashboard.firstReport.getByRole("button", {
+        name: "Edit",
+        exact: true,
+      });
+      await expect(editNewWPButton).toBeVisible();
+    } else {
+      await wpDashboard.copyoverButton.click();
+      await expect(wpDashboard.modal).toBeVisible();
+      await expect(wpDashboard.modal).toContainText("Continue");
+      await page
+        .getByRole("button", { name: "Continue from previous period" })
+        .click();
+
+      // Confirm created WP is in table
+      await wpDashboard.getReports();
+      await expect(wpDashboard.copiedReport).toBeVisible();
+      const editCopiedReportButton = wpDashboard.copiedReport.getByRole(
+        "button",
+        {
+          name: "Edit",
+          exact: true,
+        }
+      );
+      await expect(editCopiedReportButton).toBeVisible();
+    }
+  });
+
+  test("State user can fill and submit a Work Plan", async ({
+    page,
+    stateHomePage,
+    wpDashboard,
+    wpGeneralInformation,
+    wpTransitionBenchmarksProjections,
+    wpTransitionBenchmarkStrategy,
+    wpInitiativesInstructions,
+    wpInitiativesDashboard,
+    wpReviewAndSubmit,
+  }) => {
+    await logInStateUser(page);
+    await stateHomePage.wpButton.click();
+
+    // Dashboard
+    await wpDashboard.isReady();
     const editNewWPButton = wpDashboard.firstReport.getByRole("button", {
       name: "Edit",
       exact: true,
     });
-    await expect(editNewWPButton).toBeVisible();
-  } else {
-    await wpDashboard.copyoverButton.click();
-    await expect(wpDashboard.modal).toBeVisible();
-    await expect(wpDashboard.modal).toContainText("Continue");
-    await page
-      .getByRole("button", { name: "Continue from previous period" })
-      .click();
+    await editNewWPButton.click();
 
-    // Confirm created WP is in table
-    await wpDashboard.getReports();
-    await expect(wpDashboard.copiedReport).toBeVisible();
-    const editCopiedReportButton = wpDashboard.copiedReport.getByRole(
-      "button",
-      {
-        name: "Edit",
-        exact: true,
+    // General Information
+    await wpGeneralInformation.isReady();
+    await expect(wpGeneralInformation.disclosure).toBeVisible();
+
+    // Transition Benchmarks
+    await wpGeneralInformation.continueButton.click();
+    await wpTransitionBenchmarksProjections.isReady();
+    await wpTransitionBenchmarksProjections.editPopulations();
+
+    // Transition Benchmark Strategy
+    await wpTransitionBenchmarksProjections.continueButton.click();
+    await wpTransitionBenchmarkStrategy.isReady();
+    await wpTransitionBenchmarkStrategy.fillTextFields();
+
+    // Initiatives Instructions
+    await wpTransitionBenchmarkStrategy.continueButton.click();
+    await wpInitiativesInstructions.isReady();
+    await wpInitiativesInstructions.fillFormFields();
+
+    // Initiatives Dashboard
+    await wpInitiativesInstructions.continueButton.click();
+    await wpInitiativesDashboard.isReady();
+
+    if (await wpInitiativesDashboard.alert.isVisible()) {
+      for (const topic of wpInitiativesDashboard.requiredTopics) {
+        await wpInitiativesDashboard.addInitiative(topic);
       }
-    );
-    await expect(editCopiedReportButton).toBeVisible();
-  }
+    }
+
+    await expect(wpInitiativesDashboard.alert).not.toBeVisible();
+
+    // Initiatives Overlays
+    const initiatives = await wpInitiativesDashboard.page
+      .getByRole("row", {
+        name: "Edit",
+      })
+      .all();
+
+    for (const initiative of initiatives) {
+      await initiative.getByRole("button", { name: "edit button" }).click();
+      const overlayPage = new WPInitiativeOverlayPage(page);
+      await overlayPage.isReady();
+
+      await overlayPage.completeDefineInitiative();
+      await overlayPage.completeEvaluationPlan();
+      await overlayPage.completeFundingSources();
+      await overlayPage.backButton.click();
+    }
+
+    await wpInitiativesDashboard.isReady();
+
+    // Review and Submit
+    await wpInitiativesDashboard.continueButton.click();
+    await wpReviewAndSubmit.isReady();
+    const errorIcons = await wpReviewAndSubmit.page
+      .getByAltText("Error notification")
+      .all();
+    await expect(errorIcons.length).toBe(0);
+
+    await wpReviewAndSubmit.submitButton.click();
+    await wpReviewAndSubmit.confirmSubmit();
+
+    // Confirmation
+    await wpReviewAndSubmit.isReady();
+    await expect(
+      wpReviewAndSubmit.page.getByRole("heading", {
+        name: "Successfully Submitted",
+      })
+    ).toBeVisible();
+
+    await wpDashboard.goto();
+  });
 });
