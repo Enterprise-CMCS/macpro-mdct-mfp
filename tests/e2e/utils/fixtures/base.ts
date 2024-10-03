81d90e862fe98b5563701274ffca658f5dbb34d2
Jess Eldredge <jess.eldredge@coforma.io>
2024-10-03 09:54:14 -0400

    Add auth files for admin and state users

--- tests/e2e/utils/fixtures/base.ts
+++ tests/e2e/utils/fixtures/base.ts
@@ -14,11 +14,21 @@ type CustomFixtures = {
 };
 
 export const baseTest = base.extend<CustomFixtures>({
-  stateHomePage: async ({ page }, use) => {
-    await use(new StateHomePage(page));
+  stateHomePage: async ({ browser }, use) => {
+    const context = await browser.newContext({
+      storageState: "playwright/.auth/user.json",
+    });
+    const stateHomePage = new StateHomePage(await context.newPage());
+    await use(stateHomePage);
+    await context.close();
   },
-  adminHomePage: async ({ page }, use) => {
-    await use(new AdminHomePage(page));
+  adminHomePage: async ({ browser }, use) => {
+    const context = await browser.newContext({
+      storageState: "playwright/.auth/admin.json",
+    });
+    const adminHomePage = new AdminHomePage(await context.newPage());
+    await use(adminHomePage);
+    await context.close();
   },
   profilePage: async ({ page }, use) => {
     await use(new ProfilePage(page));
