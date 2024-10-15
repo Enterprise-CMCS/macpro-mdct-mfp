212836238e3ad98b3901b5a4cad1178722bf5902
Jessica Eldredge <jess.eldredge@coforma.io>
2024-10-15 09:02:13 -0400

    Spike: Storing auth for playwright tests (#779)

--- tests/e2e/wp/create.spec.ts
+++ tests/e2e/wp/create.spec.ts
@@ -1,18 +1,60 @@
+import { BrowserContext, Page } from "@playwright/test";
 import { archiveExistingWPs, logInStateUser } from "../utils";
 import { test, expect } from "../utils/fixtures/base";
 import { WPInitiativeOverlayPage } from "../utils/pageObjects/wp/wpInitiativeOverlay.page";
+import StateHomePage from "../utils/pageObjects/stateHome.page";
+import { WPDashboardPage } from "../utils/pageObjects/wp/wpDashboard.page";
+import { WPGeneralInformationPage } from "../utils/pageObjects/wp/wpGeneral.page";
+import { WPTransitionBenchmarkProjectionsPage } from "../utils/pageObjects/wp/wpTransitionBenchmarkProjections.page";
+import { WPTransitionBenchmarkStrategyPage } from "../utils/pageObjects/wp/wpTransitionBenchmarkStrategy.page";
+import { WPInitiativesInstructionsPage } from "../utils/pageObjects/wp/wpInitiativesInstructions.page";
+import { WPInitiativesDashboardPage } from "../utils/pageObjects/wp/wpInitiativesDashboard.page";
+import { WPReviewAndSubmitPage } from "../utils/pageObjects/wp/wpReviewAndSubmit.page";
+
+let userPage: Page;
+let userContext: BrowserContext;
+let homePage: StateHomePage;
+let wpDashboard: WPDashboardPage;
+let wpGeneralInformation: WPGeneralInformationPage;
+let wpTransitionBenchmarkProjections: WPTransitionBenchmarkProjectionsPage;
+let wpTransitionBenchmarkStrategy: WPTransitionBenchmarkStrategyPage;
+let wpInitiativesInstructions: WPInitiativesInstructionsPage;
+let wpInitiativesDashboard: WPInitiativesDashboardPage;
+let wpReviewAndSubmit: WPReviewAndSubmitPage;
+
+test.beforeAll(async ({ browser }) => {
+  userContext = await browser.newContext({
+    storageState: "playwright/.auth/user.json",
+  });
+  userPage = await userContext.newPage();
+
+  homePage = new StateHomePage(userPage);
+  wpDashboard = new WPDashboardPage(userPage);
+  wpGeneralInformation = new WPGeneralInformationPage(userPage);
+  wpTransitionBenchmarkProjections = new WPTransitionBenchmarkProjectionsPage(
+    userPage
+  );
+  wpTransitionBenchmarkStrategy = new WPTransitionBenchmarkStrategyPage(
+    userPage
+  );
+  wpInitiativesInstructions = new WPInitiativesInstructionsPage(userPage);
+  wpInitiativesDashboard = new WPInitiativesDashboardPage(userPage);
+  wpReviewAndSubmit = new WPReviewAndSubmitPage(userPage);
+});
+
+test.afterAll(async () => {
+  await userContext.close();
+});
 
 test.describe("Creating a new Work Plan", () => {
-  test("State user can create a Work Plan", async ({
-    page,
-    stateHomePage,
-    wpDashboard,
-  }) => {
+  test("State user can create a Work Plan", async ({ page }) => {
+    // TODO: migrate these functions to use the save auth model
     await archiveExistingWPs(page);
     await logInStateUser(page);
 
     // View WPs
-    await stateHomePage.wpButton.click();
+    await homePage.goto();
+    await homePage.wpButton.click();
     await wpDashboard.isReady();
 
     // check if work plans exist already or not
@@ -35,7 +77,7 @@ test.describe("Creating a new Work Plan", () => {
       await wpDashboard.copyoverButton.click();
       await expect(wpDashboard.modal).toBeVisible();
       await expect(wpDashboard.modal).toContainText("Continue");
-      await page
+      await wpDashboard.page
         .getByRole("button", { name: "Continue from previous period" })
         .click();
 
@@ -53,19 +95,10 @@ test.describe("Creating a new Work Plan", () => {
     }
   });
 
-  test("State user can fill and submit a Work Plan", async ({
-    page,
-    stateHomePage,
-    wpDashboard,
-    wpGeneralInformation,
-    wpTransitionBenchmarksProjections,
-    wpTransitionBenchmarkStrategy,
-    wpInitiativesInstructions,
-    wpInitiativesDashboard,
-    wpReviewAndSubmit,
-  }) => {
+  test("State user can fill and submit a Work Plan", async ({ page }) => {
     await logInStateUser(page);
-    await stateHomePage.wpButton.click();
+    await homePage.goto();
+    await homePage.wpButton.click();
 
     // Dashboard
     await wpDashboard.isReady();
@@ -81,11 +114,11 @@ test.describe("Creating a new Work Plan", () => {
 
     // Transition Benchmarks
     await wpGeneralInformation.continueButton.click();
-    await wpTransitionBenchmarksProjections.isReady();
-    await wpTransitionBenchmarksProjections.editPopulations();
+    await wpTransitionBenchmarkProjections.isReady();
+    await wpTransitionBenchmarkProjections.editPopulations();
 
     // Transition Benchmark Strategy
-    await wpTransitionBenchmarksProjections.continueButton.click();
+    await wpTransitionBenchmarkProjections.continueButton.click();
     await wpTransitionBenchmarkStrategy.isReady();
     await wpTransitionBenchmarkStrategy.fillTextFields();
 
@@ -115,12 +148,12 @@ test.describe("Creating a new Work Plan", () => {
 
     for (const initiative of initiatives) {
       await initiative.getByRole("button", { name: "edit button" }).click();
-      const overlayPage = new WPInitiativeOverlayPage(page);
+      const overlayPage = new WPInitiativeOverlayPage(userPage);
       await overlayPage.isReady();
 
-      await overlayPage.completeDefineInitiative();
-      await overlayPage.completeEvaluationPlan();
-      await overlayPage.completeFundingSources();
+      await overlayPage.completeDefineInitiative(userPage);
+      await overlayPage.completeEvaluationPlan(userPage);
+      await overlayPage.completeFundingSources(userPage);
       await overlayPage.backButton.click();
     }
 
