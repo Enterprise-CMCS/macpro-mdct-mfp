ded96fbe21c2db96f18a8e5fe2fec68d62c6a629
Bangbay Siboliban <bangbay@bluetiger.digital>
2024-11-13 14:37:50 -0500

    Destructure

--- services/ui-src/src/utils/api/apiLib.ts
+++ services/ui-src/src/utils/api/apiLib.ts
@@ -78,12 +78,13 @@ export async function apiRequest<T>(
   try {
     await updateTimeout();
 
-    const response = await request({ apiName: name, path, options }).response;
-    if (!response.body || response.statusCode === 204) {
+    const { body, statusCode } = await request({ apiName: name, path, options })
+      .response;
+    if (!body || statusCode === 204) {
       return undefined as unknown as T;
     }
 
-    return (await response.body.json()) as unknown as T;
+    return (await body.json()) as unknown as T;
   } catch (e: any) {
     const info = `Request Failed - ${path} - ${e.response?.body}`;
     console.log(e);
