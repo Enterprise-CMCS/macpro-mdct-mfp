e98bf89cc1b9011b3cc11bb997925c96259aef91
Bangbay Siboliban <bangbay@bluetiger.digital>
2024-11-14 10:54:09 -0500

    Refactor amplify methods to single file (#801)

--- services/ui-src/src/utils/api/requestMethods/banner.test.ts
+++ services/ui-src/src/utils/api/requestMethods/banner.test.ts
@@ -2,35 +2,34 @@ import { getBanner, writeBanner, deleteBanner } from "./banner";
 // utils
 import { bannerId } from "../../../constants";
 import { mockBannerData } from "utils/testing/setupJest";
-import { AnyObject } from "types";
 
-const mockDel = jest.fn();
+const mockDelete = jest.fn();
 const mockGet = jest.fn();
 const mockPost = jest.fn();
-jest.mock("../apiLib", () => ({
-  apiLib: {
-    del: (path: string, options: AnyObject) => mockDel(path, options),
-    get: (path: string, options: AnyObject) => mockGet(path, options),
-    post: (path: string, options: AnyObject) => mockPost(path, options),
-  },
+
+jest.mock("utils", () => ({
+  del: () => mockDelete(),
+  get: () => mockGet(),
+  post: () => mockPost(),
 }));
 
-describe("Test banner methods", () => {
+describe("utils/requestMethods/banner", () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
-  test("getBanner", async () => {
+
+  test("getBanner()", async () => {
     await getBanner(bannerId);
     expect(mockGet).toHaveBeenCalledTimes(1);
   });
 
-  test("postBanner", async () => {
+  test("postBanner()", async () => {
     await writeBanner(mockBannerData);
     expect(mockPost).toHaveBeenCalledTimes(1);
   });
 
-  test("delBanner", async () => {
+  test("deleteBanner()", async () => {
     await deleteBanner(bannerId);
-    expect(mockDel).toHaveBeenCalledTimes(1);
+    expect(mockDelete).toHaveBeenCalledTimes(1);
   });
 });
--- services/ui-src/src/utils/api/requestMethods/report.test.ts
+++ services/ui-src/src/utils/api/requestMethods/report.test.ts
@@ -8,65 +8,66 @@ import {
   releaseReport,
   submitReport,
 } from "./report";
-// types
-import { AnyObject } from "types";
 // utils
 import { mockReportKeys, mockWPReport } from "utils/testing/setupJest";
+import { initAuthManager } from "utils/auth/authLifecycle";
 
-const mockDel = jest.fn();
+const mockDelete = jest.fn();
 const mockGet = jest.fn();
 const mockPost = jest.fn();
 const mockPut = jest.fn();
-jest.mock("../apiLib", () => ({
-  apiLib: {
-    del: (path: string, options: AnyObject) => mockDel(path, options),
-    get: (path: string, options: AnyObject) => mockGet(path, options),
-    post: (path: string, options: AnyObject) => mockPost(path, options),
-    put: (path: string, options: AnyObject) => mockPut(path, options),
-  },
+
+jest.mock("utils", () => ({
+  del: () => mockDelete(),
+  get: () => mockGet(),
+  post: () => mockPost(),
+  put: () => mockPut(),
 }));
 
-describe("utils/report", () => {
+describe("utils/requestMethods/report", () => {
   beforeEach(() => {
+    jest.useFakeTimers();
+    initAuthManager();
+    jest.runAllTimers();
     jest.clearAllMocks();
   });
 
-  test("approveReport", async () => {
+  test("approveReport()", async () => {
     await approveReport(mockReportKeys, mockWPReport);
     expect(mockPut).toHaveBeenCalledTimes(1);
   });
 
-  test("archiveReport", async () => {
+  test("archiveReport()", async () => {
     await archiveReport(mockReportKeys);
     expect(mockPut).toHaveBeenCalledTimes(1);
   });
 
-  test("getReport", async () => {
+  test("getReport()", async () => {
     await getReport(mockReportKeys);
     expect(mockGet).toHaveBeenCalledTimes(1);
   });
 
-  test("getReportsByState", async () => {
+  test("getReportsByState()", async () => {
     await getReportsByState("WP", "NJ");
     expect(mockGet).toHaveBeenCalledTimes(1);
   });
 
-  test("postReport", async () => {
+  test("postReport()", async () => {
     await postReport("WP", "NJ", mockWPReport);
     expect(mockPost).toHaveBeenCalledTimes(1);
   });
 
-  test("putReport", async () => {
+  test("putReport()", async () => {
     await putReport(mockReportKeys, mockWPReport);
     expect(mockPut).toHaveBeenCalledTimes(1);
   });
 
-  test("releaseReport", async () => {
+  test("releaseReport()", async () => {
     await releaseReport(mockReportKeys);
     expect(mockPut).toHaveBeenCalledTimes(1);
   });
 
-  test("submitReport", async () => {
+  test("submitReport()", async () => {
     await submitReport(mockReportKeys);
     expect(mockPost).toHaveBeenCalledTimes(1);
   });
