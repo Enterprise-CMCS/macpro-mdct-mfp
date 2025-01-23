7bc4f122de616364e625de3634fc2757e9c8f438
Nicholas Summers <ntsummers1@proton.me>
2025-01-23 14:37:58 -0500

    Update onChangeHandler type for CMSDS Dropdown

--- services/ui-src/src/components/fields/DropdownField.tsx
+++ services/ui-src/src/components/fields/DropdownField.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import { useFormContext } from "react-hook-form";
-import { Dropdown as CmsdsDropdown } from "@cmsgov/design-system";
+import {
+  Dropdown as CmsdsDropdown,
+  DropdownChangeObject,
+} from "@cmsgov/design-system";
 import { Box } from "@chakra-ui/react";
 // utils
 import { labelTextWithOptional, parseCustomHtml } from "utils";
@@ -79,7 +82,7 @@ export const DropdownField = ({
   }, [hydrationValue]); // only runs on hydrationValue fetch/update
 
   // update form data
-  const onChangeHandler = async (event: InputChangeEvent) => {
+  const onChangeHandler = async (event: DropdownChangeObject) => {
     const selectedOption: SelectedOption = {
       label: event.target.name,
       value: event.target.value,
