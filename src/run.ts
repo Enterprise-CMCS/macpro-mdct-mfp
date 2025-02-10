5e337b0c40c94ab3bd229422e31bc36662709c4a
Pete Dunlap <pete.dunlap@stratiform.digital>
2025-02-10 10:49:23 -0500

    CMDCT-4324 - gets run local not needing credentials (#873)

--- src/run.ts
+++ src/run.ts
@@ -222,9 +222,7 @@ async function list_topics(options: { stage: string | undefined }) {
  * All valid arguments to dev should be enumerated here, this is the entrypoint to the script
  */
 yargs(process.argv.slice(2))
-  .command("local", "run system locally", {}, () => {
-    run_all_locally();
-  })
+  .command("local", "run system locally", {}, run_all_locally)
   .command(
     "test",
     "run all tests",
