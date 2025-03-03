ac1bbb0be807006909a81f8a0be639fd336ef7b3
Nicholas Summers <ntsummers1@proton.me>
2025-03-03 11:23:53 -0500

    Removed unneeded admin dash hint

--- services/ui-src/src/forms/adminDashSelector/adminDashSelector.ts
+++ services/ui-src/src/forms/adminDashSelector/adminDashSelector.ts
@@ -34,7 +34,6 @@ export default {
       validation: "dropdown",
       props: {
         label: "Select state or territory:",
-        hint: "This is nicks hint",
         options: dropdownOptions,
         ariaLabel:
           "List of states, including District of Columbia and Puerto Rico",
