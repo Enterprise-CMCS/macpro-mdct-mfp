223713315bf0b3ce24078abbf5056217a00fd050
Jess Eldredge <jess.eldredge@coforma.io>
2025-03-07 11:31:54 -0500

    trim back to just the initiative dashboard

--- tests/playwright/wp/create.spec.ts
+++ tests/playwright/wp/create.spec.ts
@@ -1,7 +1,15 @@
 import { BrowserContext, Page } from "@playwright/test";
 import { test, expect } from "../utils/fixtures/base";
+// import { WPInitiativeOverlayPage } from "../utils/pageObjects/wp/wpInitiativeOverlay.page";
 import StateHomePage from "../utils/pageObjects/stateHome.page";
 import { WPDashboardPage } from "../utils/pageObjects/wp/wpDashboard.page";
+import { WPGeneralInformationPage } from "../utils/pageObjects/wp/wpGeneral.page";
+import { WPTransitionBenchmarkProjectionsPage } from "../utils/pageObjects/wp/wpTransitionBenchmarkProjections.page";
+import { WPTransitionBenchmarkStrategyPage } from "../utils/pageObjects/wp/wpTransitionBenchmarkStrategy.page";
+import { WPInitiativesInstructionsPage } from "../utils/pageObjects/wp/wpInitiativesInstructions.page";
+import { WPInitiativesDashboardPage } from "../utils/pageObjects/wp/wpInitiativesDashboard.page";
+import { WPReviewAndSubmitPage } from "../utils/pageObjects/wp/wpReviewAndSubmit.page";
+import { WPInitiativeOverlayPage } from "../utils/pageObjects/wp/wpInitiativeOverlay.page";
 import AdminHomePage from "../utils/pageObjects/adminHome.page";
 
 let userPage: Page;
@@ -10,6 +18,12 @@ let adminPage: Page;
 let adminContext: BrowserContext;
 let homePage: StateHomePage;
 let wpDashboard: WPDashboardPage;
+let wpGeneralInformation: WPGeneralInformationPage;
+let wpTransitionBenchmarkProjections: WPTransitionBenchmarkProjectionsPage;
+let wpTransitionBenchmarkStrategy: WPTransitionBenchmarkStrategyPage;
+let wpInitiativesInstructions: WPInitiativesInstructionsPage;
+let wpInitiativesDashboard: WPInitiativesDashboardPage;
+let wpReviewAndSubmit: WPReviewAndSubmitPage;
 let adminHomePage: AdminHomePage;
 let adminWpDashboard: WPDashboardPage;
 
@@ -25,6 +39,16 @@ test.beforeAll(async ({ browser }) => {
 
   homePage = new StateHomePage(userPage);
   wpDashboard = new WPDashboardPage(userPage);
+  wpGeneralInformation = new WPGeneralInformationPage(userPage);
+  wpTransitionBenchmarkProjections = new WPTransitionBenchmarkProjectionsPage(
+    userPage
+  );
+  wpTransitionBenchmarkStrategy = new WPTransitionBenchmarkStrategyPage(
+    userPage
+  );
+  wpInitiativesInstructions = new WPInitiativesInstructionsPage(userPage);
+  wpInitiativesDashboard = new WPInitiativesDashboardPage(userPage);
+  // wpReviewAndSubmit = new WPReviewAndSubmitPage(userPage);
   adminHomePage = new AdminHomePage(adminPage);
   adminWpDashboard = new WPDashboardPage(adminPage);
 });
@@ -81,5 +105,96 @@ test.describe("Creating a new Work Plan", () => {
       );
       await expect(editCopiedReportButton).toBeVisible();
     }
+  });
+
+  test("State user can fill and submit a Work Plan", async () => {
+    await homePage.goto();
+    await homePage.wpButton.click();
+
+    // Dashboard
+    await wpDashboard.isReady();
+    const editNewWPButton = wpDashboard.firstReport.getByRole("button", {
+      name: "Edit",
+      exact: true,
+    });
+    await editNewWPButton.click();
+
+    // General Information
+    await wpGeneralInformation.isReady();
+    await expect(wpGeneralInformation.disclosure).toBeVisible();
+
+    // Transition Benchmarks
+    await wpGeneralInformation.continueButton.click();
+    await wpTransitionBenchmarkProjections.isReady();
+    await wpTransitionBenchmarkProjections.editPopulations();
+
+    // Transition Benchmark Strategy
+    await wpTransitionBenchmarkProjections.continueButton.click();
+    await wpTransitionBenchmarkStrategy.isReady();
+    await wpTransitionBenchmarkStrategy.fillTextFields();
+
+    // Initiatives Instructions
+    await wpTransitionBenchmarkStrategy.continueButton.click();
+    await wpInitiativesInstructions.isReady();
+    await wpInitiativesInstructions.fillFormFields();
+
+    // Initiatives Dashboard
+    await wpInitiativesInstructions.continueButton.click();
+    await wpInitiativesDashboard.isReady();
+
+    if (await wpInitiativesDashboard.alert.isVisible()) {
+      for (const topic of wpInitiativesDashboard.requiredTopics) {
+        await wpInitiativesDashboard.addInitiative(topic);
+      }
+    }
+
+    await expect(wpInitiativesDashboard.alert).not.toBeVisible();
+
+    // Initiatives Overlays
+    // const initiatives = await wpInitiativesDashboard.page
+    //   .getByRole("row", {
+    //     name: "Edit",
+    //   })
+    //   .all();
+
+    // for (const initiative of initiatives) {
+    //   await initiative.getByRole("button", { name: "edit button" }).click();
+    //   const overlayPage = new WPInitiativeOverlayPage(userPage);
+    //   await overlayPage.isReady();
+
+    //   await overlayPage.completeDefineInitiative(userPage);
+    //   await overlayPage.completeEvaluationPlan(userPage);
+    //   await overlayPage.completeFundingSources(userPage);
+    //   await overlayPage.isReady();
+    //   const errorIcons = await wpReviewAndSubmit.page
+    //     .getByAltText("warning icon")
+    //     .all();
+    //   await expect(errorIcons.length).toBe(0);
+
+    //   await overlayPage.backButton.click();
+    // }
+
+    // await wpInitiativesDashboard.isReady();
+
+    // // Review and Submit
+    // await wpInitiativesDashboard.continueButton.click();
+    // await wpReviewAndSubmit.isReady();
+    // const errorIcons = await wpReviewAndSubmit.page
+    //   .getByAltText("Error notification")
+    //   .all();
+    // await expect(errorIcons.length).toBe(0);
+
+    // await wpReviewAndSubmit.submitButton.click();
+    // await wpReviewAndSubmit.confirmSubmit();
+
+    // // Confirmation
+    // await wpReviewAndSubmit.isReady();
+    // await expect(
+    //   wpReviewAndSubmit.page.getByRole("heading", {
+    //     name: "Successfully Submitted",
+    //   })
+    // ).toBeVisible();
+
+    // await wpDashboard.goto();
   });
 });
