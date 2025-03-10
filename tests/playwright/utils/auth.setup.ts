d1bf922e59bce7ed4b77011bfee9fbb116a7b9e0
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2025-03-10 12:59:11 -0700

    Update auth.setup.ts

--- tests/playwright/utils/auth.setup.ts
+++ tests/playwright/utils/auth.setup.ts
@@ -1,115 +1,43 @@
-import {
-  APIResponse,
-  Browser,
-  Page,
-  test as setup,
-  expect,
-} from "@playwright/test";
-import {
-  adminAuthPath,
-  adminPassword,
-  adminUser,
-  statePassword,
-  stateUser,
-  stateUserAuthPath,
-} from "./consts";
+import { test as setup } from "@playwright/test";
 
-async function isAlreadyLoggedIn(page) {
-  try {
-    await Promise.race([
-      page
-        .waitForResponse(
-          (response: APIResponse) =>
-            response.url().includes("assets") && response.status() === 200,
-          { timeout: 1000 }
-        )
-        .catch(() => {}),
-      page.waitForTimeout(1000),
-    ]);
-    // eslint-disable-next-line no-empty
-  } catch {}
+import { adminPassword, adminUser, statePassword, stateUser } from "./consts";
 
-  return !(await page
-    .getByRole("textbox", { name: "email" })
-    .isVisible()
-    .catch(() => false));
-}
+const adminFile = "playwright/.auth/admin.json";
 
-async function getContext(browser: Browser, storageStatePath: string) {
-  try {
-    return await browser.newContext({ storageState: storageStatePath });
-  } catch {
-    return await browser.newContext();
-  }
-}
-
-setup("authenticate as admin", async ({ browser }) => {
-  const context = await getContext(browser, adminAuthPath);
-
-  const page = await context.newPage();
+setup("authenticate as admin", async ({ page }) => {
   await page.goto("/");
-
-  if (await isAlreadyLoggedIn(page)) {
-    return;
-  }
-
-  await loginUser(
-    page,
-    adminUser,
-    adminPassword,
-    "View State/Territory Reports"
-  );
-  await page.context().storageState({ path: adminAuthPath });
-});
-
-setup("authenticate as user", async ({ browser }) => {
-  const context = await getContext(browser, stateUserAuthPath);
-
-  const page = await context.newPage();
-  await page.goto("/");
-
-  if (await isAlreadyLoggedIn(page)) {
-    return;
-  }
-
-  await loginUser(
-    page,
-    stateUser,
-    statePassword,
-    "Money Follows the Person (MFP) Portal"
-  );
-  await page.context().storageState({ path: stateUserAuthPath });
-});
-
-async function waitForJwtResponse(page: Page) {
-  await page.waitForResponse(async (response) => {
-    if (
-      response.url() === "https://cognito-idp.us-east-1.amazonaws.com/" &&
-      response.request().method() === "POST" &&
-      response.ok()
-    ) {
-      const data = await response.json();
-      return (
-        data?.AuthenticationResult && data.AuthenticationResult.AccessToken
-      );
-    }
-    return false;
-  });
-}
-
-async function loginUser(
-  page: Page,
-  email: string,
-  password: string,
-  finalHeading: string
-) {
   const emailInput = page.getByRole("textbox", { name: "email" });
   const passwordInput = page.getByRole("textbox", { name: "password" });
   const loginButton = page.getByRole("button", { name: "Log In with Cognito" });
-  await emailInput.fill(email);
-  await passwordInput.fill(password);
+  await emailInput.fill(adminUser);
+  await passwordInput.fill(adminPassword);
   await loginButton.click();
   await page.waitForURL("/");
-  await waitForJwtResponse(page);
-  await expect(page.getByRole("heading", { name: finalHeading })).toBeVisible();
-}
+  await page
+    .getByRole("heading", {
+      name: "View State/Territory Reports",
+    })
+    .isVisible();
+  await page.waitForTimeout(1000);
+  await page.context().storageState({ path: adminFile });
+});
+
+const userFile = "playwright/.auth/user.json";
+
+setup("authenticate as user", async ({ page }) => {
+  await page.goto("/");
+  const emailInput = page.getByRole("textbox", { name: "email" });
+  const passwordInput = page.getByRole("textbox", { name: "password" });
+  const loginButton = page.getByRole("button", { name: "Log In with Cognito" });
+  await emailInput.fill(stateUser);
+  await passwordInput.fill(statePassword);
+  await loginButton.click();
+  await page.waitForURL("/");
+  await page
+    .getByRole("heading", {
+      name: "Money Follows the Person (MFP) Portal",
+    })
+    .isVisible();
+  await page.waitForTimeout(1000);
+  await page.context().storageState({ path: userFile });
+});
