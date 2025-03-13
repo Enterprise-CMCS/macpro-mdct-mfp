c4c2df489c17502be6cc788bb34333ac3ddd12bf
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2025-03-12 21:14:36 -0600

    Update write-ui-env-file.ts

--- src/write-ui-env-file.ts
+++ src/write-ui-env-file.ts
@@ -10,6 +10,21 @@ const configFilePath = path.resolve(outputPath, "env-config.js");
 
 const region = "us-east-1";
 
+// TODO: update the below for these additional variables
+// export APPLICATION_ENDPOINT=${self:custom.application_endpoint}
+// export COGNITO_IDP_NAME=${self:custom.cognito_idp_name}
+// export COGNITO_IDP=${self:custom.cognito_idp}
+
+// export COGNITO_REDIRECT_SIGNOUT=${self:custom.application_signout_url}
+// export POST_SIGNOUT_REDIRECT=${self:custom.signout_redirect_url}
+// export REACT_APP_LD_SDK_CLIENT=${self:custom.ldSdkClient}
+// export S3_ATTACHMENTS_BUCKET_REGION=${self:custom.s3_attachments_bucket_region}
+// export S3_ATTACHMENTS_BUCKET_NAME=${self:custom.s3_attachments_bucket_name}
+
+// export LOCAL_LOGIN=false
+// ./scripts/configure-env.sh
+// cp public/env-config.js build/env-config.js
+
 export async function writeLocalUiEnvFile(apiUrl: string) {
   const envVariables = {
     LOCAL_LOGIN: "false",
