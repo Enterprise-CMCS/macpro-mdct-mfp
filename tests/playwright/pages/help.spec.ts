134578306c72a168aea1c22ae98471957fba63e8
Jess Eldredge <jess.eldredge@coforma.io>
2025-03-18 13:32:49 -0400

    Use stored auth for the help page test

--- tests/playwright/pages/help.spec.ts
+++ tests/playwright/pages/help.spec.ts
@@ -1,14 +1,22 @@
 import { test } from "@playwright/test";
-import { e2eA11y } from "../utils";
+import { e2eA11y, stateUserAuthPath, adminAuthPath } from "../utils";
 
-test("Help page is accessible on all device types for state user", async ({
-  page,
-}) => {
-  await e2eA11y(page, "/help");
+test.describe("State user help page", () => {
+  test.use({ storageState: stateUserAuthPath });
+
+  test("Help page is accessible on all device types for state user", async ({
+    page,
+  }) => {
+    await e2eA11y(page, "/help");
+  });
 });
 
-test("Help page is accessible on all device types for admin user", async ({
-  page,
-}) => {
-  await e2eA11y(page, "/help");
+test.describe("Admin user help page", () => {
+  test.use({ storageState: adminAuthPath });
+
+  test("Help page is accessible on all device types for admin user", async ({
+    page,
+  }) => {
+    await e2eA11y(page, "/help");
+  });
 });
