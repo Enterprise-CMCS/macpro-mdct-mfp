9c7da8a93496817a8ed20274856cbd614ae39252
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2025-03-20 07:32:31 -0700

    Update env-config.template.js

--- deployment/stacks/env-config.template.js
+++ deployment/stacks/env-config.template.js
@@ -1,15 +1,20 @@
 window._env_ = {
-  LOCAL_LOGIN: "false",
-  SKIP_PREFLIGHT_CHECK: "true",
   API_REGION: "us-east-1",
   API_URL: "{{apiGatewayRestApiUrl}}",
-  COGNITO_REGION: "us-east-1",
+  APPLICATION_ENDPOINT: "{{applicationEndpointUrl}}",
+  COGNITO_IDP_NAME: "Okta",
   COGNITO_IDENTITY_POOL_ID: "{{identityPoolId}}",
-  COGNITO_USER_POOL_ID: "{{userPoolId}}",
+  COGNITO_REDIRECT_SIGNIN: "{{applicationEndpointUrl}}",
+  COGNITO_REDIRECT_SIGNOUT: "{{applicationEndpointUrl}}postLogout",
+  COGNITO_REGION: "us-east-1",
   COGNITO_USER_POOL_CLIENT_ID: "{{userPoolClientId}}",
   COGNITO_USER_POOL_CLIENT_DOMAIN: "{{userPoolClientDomain}}",
-  COGNITO_REDIRECT_SIGNIN: "{{applicationEndpointUrl}}",
-  COGNITO_REDIRECT_SIGNOUT: "{{applicationEndpointUrl}}",
+  COGNITO_USER_POOL_ID: "{{userPoolId}}",
+  LOCAL_LOGIN: "false",
+  POST_SIGNOUT_REDIRECT: "{{applicationEndpointUrl}}",
+  REACT_APP_LD_SDK_CLIENT: "{{launchDarklyClient}}", // TODO
+  S3_ATTACHMENTS_BUCKET_NAME: "{{s3AttachmentsBucketName}}", // TODO
+  S3_ATTACHMENTS_BUCKET_REGION: "{{s3AttachmentsBucketRegion}}", // TODO
   STAGE: "{{stage}}",
   TIMESTAMP: "{{timestamp}}",
 };
