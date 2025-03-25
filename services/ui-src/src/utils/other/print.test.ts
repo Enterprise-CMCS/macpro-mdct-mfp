072bd1285c0ef0f90578dac78edb477b8050ff8d
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2025-03-25 09:09:44 -0700

    remove DEV_API_URL from print.test.ts

--- services/ui-src/src/utils/other/print.test.ts
+++ services/ui-src/src/utils/other/print.test.ts
@@ -8,7 +8,6 @@ jest.mock("utils/api/apiLib", () => ({
 }));
 
 const testBody = "<noscript>removed</noscript><h1>Hello</h1><";
-const originalURLConfig = config.DEV_API_URL;
 window.open = jest.fn();
 
 const path = "/print_pdf";
@@ -28,29 +27,14 @@ describe("utils/print", () => {
     });
 
     afterAll(() => {
-      config.DEV_API_URL = originalURLConfig;
       jest.resetAllMocks();
     });
 
-    test("Call to the dev api if an env flag is provided", async () => {
-      mockPost.mockImplementation(() =>
-        Buffer.from(testBody).toString("base64")
-      );
-      config.DEV_API_URL = "test.com";
-      document.body.innerHTML = testBody;
-      await printPdf();
-
-      expect(mockPost).toHaveBeenCalledTimes(1);
-      expect(mockPost).toHaveBeenCalledWith(path, options, apiName);
-      expect(window.open).toBeCalled();
-    });
-
-    test("Calls normal API env flag is not provided", async () => {
+    test("Calls API", async () => {
       mockPost.mockImplementation(() =>
         Buffer.from(testBody).toString("base64")
       );
 
-      config.DEV_API_URL = null;
       await printPdf();
 
       expect(mockPost).toHaveBeenCalledTimes(1);
