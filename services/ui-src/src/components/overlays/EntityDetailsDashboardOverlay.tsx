21cdae288c89bcd18277eb2bfb627b0919d43083
Garrett Rabian <57802560+gmrabian@users.noreply.github.com>
2025-07-16 13:50:33 -0400

    Fix colors (mdct prefix) (#990)

--- services/ui-src/src/components/overlays/EntityDetailsDashboardOverlay.tsx
+++ services/ui-src/src/components/overlays/EntityDetailsDashboardOverlay.tsx
@@ -186,7 +186,7 @@ const sx = {
   },
   footerBox: {
     marginTop: "2rem",
-    borderTop: "1.5px solid var(--chakra-colors-palette-gray_light)",
+    borderTop: "1.5px solid var(--mdct-colors-palette-gray_light)",
   },
   buttonFlex: {
     justifyContent: "end",
