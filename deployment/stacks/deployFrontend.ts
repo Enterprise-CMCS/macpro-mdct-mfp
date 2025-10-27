b282e5edd8af5bd6101291ca80d870355a1168e4
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2025-10-27 15:39:21 -0400

    testing without explicit invalidate, using auto invalidate from deploy

--- deployment/stacks/deployFrontend.ts
+++ deployment/stacks/deployFrontend.ts
@@ -1,11 +1,9 @@
 import { Construct } from "constructs";
 import {
-  aws_iam as iam,
   aws_cloudfront as cloudfront,
-  Duration,
   aws_s3 as s3,
   aws_s3_deployment as s3_deployment,
-  custom_resources as cr,
+  Duration,
 } from "aws-cdk-lib";
 import path from "path";
 import { execSync } from "node:child_process";
@@ -28,7 +26,6 @@ interface DeployFrontendProps {
 export function deployFrontend(props: DeployFrontendProps) {
   const {
     scope,
-    stage,
     distribution,
     apiGatewayRestApiUrl,
     applicationEndpointUrl,
@@ -50,12 +47,6 @@ export function deployFrontend(props: DeployFrontendProps) {
     stdio: "inherit",
   });
 
-  const deploymentRole = new iam.Role(scope, "BucketDeploymentRole", {
-    assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
-  });
-
-  uiBucket.grantReadWrite(deploymentRole);
-
   const deployWebsite = new s3_deployment.BucketDeployment(
     scope,
     "DeployWebsite",
@@ -68,11 +59,8 @@ export function deployFrontend(props: DeployFrontendProps) {
       cacheControl: [
         s3_deployment.CacheControl.setPublic(),
         s3_deployment.CacheControl.maxAge(Duration.days(365)),
-        s3_deployment.CacheControl.noCache(),
       ],
-      role: deploymentRole,
-    }
-  );
+    });
 
   const deployTimeConfig = new s3_deployment.DeployTimeSubstitutedFile(
     scope,
@@ -92,38 +80,8 @@ export function deployFrontend(props: DeployFrontendProps) {
         launchDarklyClient,
         redirectSignout,
       },
-      role: deploymentRole,
     }
   );
 
-  deployTimeConfig.node.addDependency(deployWebsite);
-
-  const invalidateCloudfront = new cr.AwsCustomResource(
-    scope,
-    "InvalidateCloudfront",
-    {
-      onUpdate: {
-        service: "CloudFront",
-        action: "createInvalidation",
-        parameters: {
-          DistributionId: distribution.distributionId,
-          InvalidationBatch: {
-            Paths: {
-              Quantity: 1,
-              Items: ["/*"],
-            },
-            CallerReference: new Date().toISOString(),
-          },
-        },
-        physicalResourceId: cr.PhysicalResourceId.of(
-          `InvalidateCloudfront-${stage}`
-        ),
-      },
-      role: deploymentRole,
-    }
-  );
-
-  distribution.grantCreateInvalidation(invalidateCloudfront.grantPrincipal);
-
-  invalidateCloudfront.node.addDependency(deployTimeConfig);
+  deployWebsite.node.addDependency(deployTimeConfig);
 }
