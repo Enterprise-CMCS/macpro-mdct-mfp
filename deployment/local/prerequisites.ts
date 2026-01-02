a9fc8b7317380bb5bd0116cba2f3137529d5de99
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2026-01-02 12:51:17 -0500

    fix orphaned ephemeral log groups (#1227)

--- deployment/local/prerequisites.ts
+++ deployment/local/prerequisites.ts
@@ -30,7 +30,7 @@ export class LocalPrerequisiteStack extends Stack {
     });
 
     new secretsmanager.Secret(this, "DefaultSecret", {
-      secretName: "mfp-default", // pragma: allowlist secret
+      secretName: `${process.env.PROJECT}-default`, // pragma: allowlist secret
       secretObjectValue: {
         vpcName: SecretValue.unsafePlainText("localstack-dev"),
         brokerString: SecretValue.unsafePlainText("localstack"),
@@ -68,7 +68,7 @@ export class LocalPrerequisiteStack extends Stack {
 async function main() {
   const app = new App();
 
-  new LocalPrerequisiteStack(app, "mfp-local-prerequisites");
+  new LocalPrerequisiteStack(app, `${process.env.PROJECT}-local-prerequisites`);
 }
 
 main();
