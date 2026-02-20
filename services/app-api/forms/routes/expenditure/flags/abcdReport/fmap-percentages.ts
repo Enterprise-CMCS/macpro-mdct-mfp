11f9cc0768dad1300f64c2138bb1fa685e22b002
Ajaita Saini <ajaita.saini@coforma.io>
2026-02-20 12:43:39 -0500

    content feedback

--- services/app-api/forms/routes/expenditure/flags/abcdReport/fmap-percentages.ts
+++ services/app-api/forms/routes/expenditure/flags/abcdReport/fmap-percentages.ts
@@ -16,52 +16,62 @@ export const fmapPercentagesRoute: FormRoute = {
       subsection: "FMAP Percentages",
       info: [
         {
-          type: "html",
-          content:
-            "Enter the Enhanced Federal Medical Assistance Percentage (E-FMAP) for your state for each category below. Please refer to the FMAP rates published in the ",
-        },
-        {
-          type: "externalLink",
-          content: "Federal Register",
-          props: {
-            href: "https://www.federalregister.gov/",
-            target: "_blank",
-            "aria-label": "Federal Register (Link opens in new tab)",
-          },
-        },
-        {
-          type: "html",
-          content:
-            " for the appropriate year. Fiscal year 2025 FMAP rates are available at  ",
-        },
-        {
-          type: "externalLink",
-          content: "https://www.federalregister.gov/d/2023-25636",
-          props: {
-            href: "https://www.federalregister.gov/d/2023-25636",
-            target: "_blank",
-            "aria-label":
-              "Federal Register Fiscal year 2025 FMAP rates (Link opens in new tab)",
-          },
-        },
-        {
-          type: "html",
-          content: ". Fiscal year 2026 FMAP rates are available at ",
-        },
-        {
-          type: "externalLink",
-          content: "https://www.federalregister.gov/d/2024-27910",
-          props: {
-            href: "https://www.federalregister.gov/d/2024-27910",
-            target: "_blank",
-            "aria-label":
-              "Federal Register Fiscal year 2026 FMAP rates (Link opens in new tab)",
-          },
-        },
-        {
-          type: "html",
-          content:
-            ". Additional information about the MFP-Enhanced FMAP can be found in the MFP Program Terms and Conditions (PTC), specifically PTC 22. <br><br> In the sections that follow, your state or territory’s share for each service will be calculated using the Enhanced FMAP that you provide here.",
+          type: "p",
+          children: [
+            {
+              type: "html",
+              content:
+                "Enter the Enhanced Federal Medical Assistance Percentage (E-FMAP) for your state for each category below. Please refer to the FMAP rates published in the ",
+            },
+            {
+              type: "externalLink",
+              content: "Federal Register",
+              props: {
+                href: "https://www.federalregister.gov/",
+                target: "_blank",
+                "aria-label": "Federal Register (Link opens in new tab)",
+              },
+            },
+            {
+              type: "html",
+              content:
+                " for the appropriate year. Fiscal year 2025 FMAP rates are available at  ",
+            },
+            {
+              type: "externalLink",
+              content: "https://www.federalregister.gov/d/2023-25636",
+              props: {
+                href: "https://www.federalregister.gov/d/2023-25636",
+                target: "_blank",
+                "aria-label":
+                  "Federal Register Fiscal year 2025 FMAP rates (Link opens in new tab)",
+              },
+            },
+            {
+              type: "html",
+              content: ". Fiscal year 2026 FMAP rates are available at ",
+            },
+            {
+              type: "externalLink",
+              content: "https://www.federalregister.gov/d/2024-27910",
+              props: {
+                href: "https://www.federalregister.gov/d/2024-27910",
+                target: "_blank",
+                "aria-label":
+                  "Federal Register Fiscal year 2026 FMAP rates (Link opens in new tab)",
+              },
+            },
+            {
+              type: "html",
+              content:
+                ". Additional information about the MFP-Enhanced FMAP can be found in the MFP Program Terms and Conditions (PTC), specifically PTC 22.",
+            },
+            {
+              type: "p",
+              content:
+                "In the sections that follow, your state or territory’s share for each service will be calculated using the Enhanced FMAP that you provide here.",
+            },
+          ],
         },
       ],
     },
