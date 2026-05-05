919d73ce7bd41dca865629b25c9320b2074d0c4c
Britt <britt.bujacich@gmail.com>
2026-05-05 14:20:52 -0600

    simply MainSkipNav, remove SkipNav in Sidebar (#1434)

--- services/ui-src/src/components/app/MainSkipNav.test.tsx
+++ services/ui-src/src/components/app/MainSkipNav.test.tsx
@@ -1,71 +1,21 @@
 import { render, screen } from "@testing-library/react";
 //components
-import { MainSkipNav, ReportContext } from "components";
-import { ReportContextShape } from "types";
+import { MainSkipNav } from "components";
 import { testA11yAct } from "utils/testing/commonTests";
 
-const mockUseLocation = jest.fn();
-jest.mock("react-router", () => ({
-  useLocation: () => mockUseLocation(),
-}));
-
-const mainSkipNavOutsideReport = (
-  <ReportContext.Provider
-    value={
-      {
-        isReportPage: false,
-      } as ReportContextShape
-    }
-  >
-    <MainSkipNav />
-  </ReportContext.Provider>
-);
-
-const mainSkipNavInsideReport = (
-  <ReportContext.Provider
-    value={
-      {
-        isReportPage: true,
-      } as ReportContextShape
-    }
-  >
-    <MainSkipNav />
-  </ReportContext.Provider>
-);
+const mainSkipNav = <MainSkipNav />;
 
 describe("<MainSkipNav />", () => {
   test("should be visible and focusable", async () => {
-    mockUseLocation.mockReturnValue({ pathname: "/home" });
-    render(mainSkipNavOutsideReport);
+    render(mainSkipNav);
     const skipNav = document.getElementById("skip-nav-main")!;
     skipNav.focus();
 
     const skipNavLink = screen.getByText("Skip to main content");
     await expect(skipNavLink).toHaveFocus();
     await expect(skipNavLink).toBeVisible();
+    await expect(skipNavLink).toHaveAttribute("href", "#main-content");
   });
 
-  test("should skip to report content when on a report page", async () => {
-    mockUseLocation.mockReturnValue({ pathname: "/report-page" });
-    render(mainSkipNavInsideReport);
-    const skipNav = document.getElementById("skip-nav-main")!;
-    skipNav.focus();
-
-    const skipNavLink = screen.getByText("Skip to report sidebar");
-    await expect(skipNavLink).toHaveFocus();
-    await expect(skipNavLink).toBeVisible();
-  });
-
-  test("should skip to main content when on an export page", async () => {
-    mockUseLocation.mockReturnValue({ pathname: "/export" });
-    render(mainSkipNavInsideReport);
-    const skipNav = document.getElementById("skip-nav-main")!;
-    skipNav.focus();
-
-    const skipNavLink = screen.getByText("Skip to main content");
-    await expect(skipNavLink).toHaveFocus();
-    await expect(skipNavLink).toBeVisible();
-  });
-
-  testA11yAct(mainSkipNavOutsideReport);
+  testA11yAct(mainSkipNav);
 });
